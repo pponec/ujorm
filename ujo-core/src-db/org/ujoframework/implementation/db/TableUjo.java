45277d09b20b3bfbd90b054ec0e5b15789cf4c4e
ponec <ponec@localhost>
2009-01-25 15:17:17 +0000

    DB persistence by an ORM mapping (JavaDoc)

--- ujo-core/src-db/org/ujoframework/implementation/db/TableUjo.java
+++ ujo-core/src-db/org/ujoframework/implementation/db/TableUjo.java
@@ -16,7 +16,6 @@
 
 package org.ujoframework.implementation.db;
 
-import java.beans.Expression;
 import org.ujoframework.Ujo;
 import org.ujoframework.UjoProperty;
 import org.ujoframework.beans.EventRegistrar;
@@ -25,8 +24,97 @@ import org.ujoframework.beans.UjoPropertyChangeListener;
 import org.ujoframework.implementation.map.MapUjo;
 
 /**
- * An implementation for an ORM solution.
+ * A simple implementation of an ORM solution. Why new ORM mapping?
+ * <ul>
+ *    <li>type safe query language!</li>
+ *    <li>no more a LazyInitialization exception :-)</li>
+ *    <li>no confusing proxy business objects</li>
+ *    <li>no more list properties, for a collection is designed by a new object called <a href="../../core/UjoIterator.html">UjoIterator</a>. The List object provides a toList() method however, if you need it</li>
+ *    <li>very small framework without library dependences</li>
+ * </ul>
+ * Some other features:
+ * <ul>
+ *    <li>the API was inspired by a Cayenne and Hibernate ORM frameworks</li>
+ *    <li>missing cache is siutable for a large transactions and a selecting uncommited data</li>
+ *    <li>all persistent objects are based on the Ujo interface, specialy on a TableUjo implementation</li>
+ *    <li>default ORM mapping is based on the UjoProperty names however there is possible overwrite the mapping by annotations and the annoatations can be owerwrited by a XML files </li>
+ * </ul>
+ * The sample of use:
+ * <pre class="pre">
+ *  <span class="comment">&#47;**</span> <span class="comment">Using</span> <span class="comment">INSERT</span> <span class="comment">*&#47;</span>
+ *  <span class="keyword-directive">public</span> <span class="keyword-directive">void</span> useCreateItem() {
+ *
+ *      Session session = DbHandler.getInstance().getSession();
+ *
+ *      BoOrder order = <span class="keyword-directive">new</span> BoOrder();
+ *      BoOrder.DATE.setValue(order, <span class="keyword-directive">new</span> Date());
+ *      BoOrder.DESCR.setValue(order, <span class="character">&quot;</span><span class="character">My first order</span><span class="character">&quot;</span>);
+ *
+ *      BoItem item = <span class="keyword-directive">new</span> BoItem();
+ *      BoItem.DESCR.setValue(item, <span class="character">&quot;</span><span class="character">yellow table</span><span class="character">&quot;</span>);
+ *      BoItem.ORDER.setValue(item, order);
+ *
+ *
+ *      session.save(order);
+ *      session.save(item);
+ *
+ *      <span class="keyword-directive">if</span> (<span class="keyword-directive">true</span>) {
+ *         session.commit();
+ *      } <span class="keyword-directive">else</span> {
+ *         session.rollback();
+ *      }
+ *  }
+ *
+ *  <span class="comment">&#47;**</span> <span class="comment">Using</span> <span class="comment">SELECT</span> <span class="comment">by</span> <span class="comment">a</span> <span class="comment">object</span> <span class="comment">relations</span> <span class="comment">*&#47;</span>
+ *  <span class="keyword-directive">public</span> <span class="keyword-directive">void</span> useRelation() {
+ *
+ *      Session session = DbHandler.getInstance().getSession();
+ *      BoDatabase db = session.getDatabase();
+ *
+ *      UjoIterator&lt;BoOrder&gt; orders  = BoDatabase.ORDERS.of(db);
+ *      <span class="keyword-directive">for</span> (BoOrder order : orders) {
+ *          Long id = BoOrder.ID.of(order);
+ *          String descr = BoOrder.DESCR.of(order);
+ *          System.out.println(<span class="character">&quot;</span><span class="character">Order id: </span><span class="character">&quot;</span> + id + <span class="character">&quot;</span><span class="character"> descr: </span><span class="character">&quot;</span> + descr);
+ *
+ *          <span class="keyword-directive">for</span> (BoItem item : BoOrder.ITEMS.of(order)) {
+ *              Long itemId = BoItem.ID.of(item);
+ *              String itemDescr = BoItem.DESCR.of(item);
+ *              System.out.println(<span class="character">&quot;</span><span class="character"> Item id: </span><span class="character">&quot;</span> + itemId + <span class="character">&quot;</span><span class="character"> descr: </span><span class="character">&quot;</span> + itemDescr);
+ *          }
+ *      }
+ *  }
+ *
+ *
+ *  <span class="comment">&#47;**</span> <span class="comment">Using</span> <span class="comment">SELECT</span> <span class="comment">by</span> <span class="comment">QUERY</span> <span class="comment">*&#47;</span>
+ *  <span class="keyword-directive">public</span> <span class="keyword-directive">void</span> useSelection() {
+ *
+ *      Session session = DbHandler.getInstance().getSession();
+ *
+ *      Expression&lt;BoOrder&gt; exp1 = Expression.newInstance(BoOrder.ID);
+ *      Expression&lt;BoOrder&gt; exp2 = Expression.newInstance(BoOrder.DATE);
+ *      Expression&lt;BoOrder&gt; expr = exp1.and(exp2);
+ *
+ *      Query&lt;BoOrder&gt; query = session.createQuery(BoOrder.<span class="keyword-directive">class</span>, expr);
+ *      query.sizeRequired(<span class="keyword-directive">true</span>);
+ *      query.readOnly(<span class="keyword-directive">false</span>);
+ *      query.setParameter(BoOrder.ID, 10L);
+ *      query.setParameter(BoOrder.DATE, <span class="keyword-directive">new</span> Date());
+ *
+ *      BoOrder order = session.single(query);
+ *
+ *      <span class="keyword-directive">for</span> (BoOrder o : session.iterate( query ) ) {
+ *          Long id = BoOrder.ID.of(order);
+ *          String descr = BoOrder.DESCR.of(order);
+ *          System.out.println(<span class="character">&quot;</span><span class="character">Order id: </span><span class="character">&quot;</span> + id + <span class="character">&quot;</span><span class="character"> descr: </span><span class="character">&quot;</span> + descr);
+ *      }
+ *  }</pre>
+ *
+ * <strong>Note</strong>: the API desing a very early prototype, methods are not implemented yet.
+ *
  * @author Ponec
+ * @see org.ujoframework.implementation.db.UjoRelative
+ * @see org.ujoframework.core.UjoIterator
  */
 public class TableUjo<UJO extends Ujo> extends MapUjo implements EventRegistrar<UJO> {
     
