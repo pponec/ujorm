ef9783fd18ebfd0aa8d18e397e71089f6f5e00a9
ponec <ponec@localhost>
2009-01-25 20:39:34 +0000

    DB persistence by an ORM mapping (JavaDoc + code cleaning)

--- ujo-core/src-db/org/ujoframework/implementation/db/sample/BoItem.java
+++ ujo-core/src-db/org/ujoframework/implementation/db/sample/BoItem.java
@@ -9,15 +9,20 @@ import org.ujoframework.UjoProperty;
 import org.ujoframework.implementation.db.TableUjo;
 
 /**
- *
+ * Each Item have got a reference to a common Order.
  * @Table=bo_item
  */
 public class BoItem extends TableUjo {
 
-    /** @id */
+    /** Unique key
+     * @id
+     */
     public static final UjoProperty<BoItem,Long> ID = newProperty("id", Long.class);
+    /** User key */
     public static final UjoProperty<BoItem,Integer> USER_ID = newProperty("usrId", Integer.class);
+    /** Description of Item */
     public static final UjoProperty<BoItem,String> DESCR = newProperty("description", String.class);
+    /** A reference to common Order */
     public static final UjoProperty<BoItem,BoOrder> ORDER = newProperty("order", BoOrder.class);
 
 
