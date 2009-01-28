859e30ac221ceced390cd31998fe28375fa45d65
ponec <ponec@localhost>
2009-01-28 17:46:06 +0000

    JavaDoc

--- ujo-core/src-db/org/ujoframework/implementation/db/TableUjo.java
+++ ujo-core/src-db/org/ujoframework/implementation/db/TableUjo.java
@@ -30,7 +30,7 @@ import org.ujoframework.implementation.map.MapUjo;
  *    <li>type safe query language!</li>
  *    <li>no more a LazyInitialization exception :-)</li>
  *    <li>no confusing proxy business objects</li>
- *    <li>no more list properties, a new object called <a href="../../core/UjoIterator.html">UjoIterator</a>  is designed for a collection. The UjoIterator provides a toList() method however</li>
+ *    <li>no more list properties, a new object called <a href="../../core/UjoIterator.html">UjoIterator</a> is designed for a collection. The UjoIterator provides a toList() method however</li>
  *    <li>very small framework without more library dependences</li>
  * </ul>
  * Some other features:
@@ -44,19 +44,18 @@ import org.ujoframework.implementation.map.MapUjo;
  * </ul>
  * The sample of use:
  * <pre class="pre">
- *  <span class="comment">&#47;**</span> <span class="comment">Using</span> <span class="comment">INSERT</span> <span class="comment">*&#47;</span>
+ *  <span class="comment">&#47;** Using INSERT *&#47;</span>
  *  <span class="keyword-directive">public</span> <span class="keyword-directive">void</span> useCreateItem() {
  *
  *      Session session = DbHandler.getInstance().getSession();
  *
  *      BoOrder order = <span class="keyword-directive">new</span> BoOrder();
  *      BoOrder.DATE.setValue(order, <span class="keyword-directive">new</span> Date());
- *      BoOrder.DESCR.setValue(order, <span class="character">&quot;</span><span class="character">My first order</span><span class="character">&quot;</span>);
+ *      BoOrder.DESCR.setValue(order, <span class="character">&quot;</span><span class="character">test order</span><span class="character">&quot;</span>);
  *
  *      BoItem item = <span class="keyword-directive">new</span> BoItem();
  *      BoItem.DESCR.setValue(item, <span class="character">&quot;</span><span class="character">yellow table</span><span class="character">&quot;</span>);
  *      BoItem.ORDER.setValue(item, order);
- *
  *
  *      session.save(order);
  *      session.save(item);
@@ -68,13 +67,13 @@ import org.ujoframework.implementation.map.MapUjo;
  *      }
  *  }
  *
- *  <span class="comment">&#47;**</span> <span class="comment">Using</span> <span class="comment">SELECT</span> <span class="comment">by</span> <span class="comment">a</span> <span class="comment">object</span> <span class="comment">relations</span> <span class="comment">*&#47;</span>
+ *  <span class="comment">&#47;** Using SELECT by a object relations *&#47;</span>
  *  <span class="keyword-directive">public</span> <span class="keyword-directive">void</span> useRelation() {
  *
  *      Session session = DbHandler.getInstance().getSession();
  *      BoDatabase db = session.getDatabase();
  *
- *      UjoIterator&lt;BoOrder&gt; orders  = BoDatabase.ORDERS.of(db);
+ *      UjoIterator&lt;BoOrder&gt; orders = BoDatabase.ORDERS.of(db);
  *      <span class="keyword-directive">for</span> (BoOrder order : orders) {
  *          Long id = BoOrder.ID.of(order);
  *          String descr = BoOrder.DESCR.of(order);
@@ -88,7 +87,7 @@ import org.ujoframework.implementation.map.MapUjo;
  *      }
  *  }
  *
- *  <span class="comment">&#47;**</span> <span class="comment">Using</span> <span class="comment">SELECT</span> <span class="comment">by</span> <span class="comment">QUERY</span> <span class="comment">*&#47;</span>
+ *  <span class="comment">&#47;** Using SELECT by QUERY *&#47;</span>
  *  <span class="keyword-directive">public</span> <span class="keyword-directive">void</span> useSelection() {
  *
  *      Session session = DbHandler.getInstance().getSession();
@@ -101,7 +100,7 @@ import org.ujoframework.implementation.map.MapUjo;
  *      query.sizeRequired(<span class="keyword-directive">true</span>); <span class="comment">// need a count of iterator items, a default value is false</span>
  *      query.readOnly(<span class="keyword-directive">false</span>);
  *
- *      <span class="keyword-directive">for</span> (BoOrder order : session.iterate( query ) ) {
+ *      <span class="keyword-directive">for</span> (BoOrder order : session.iterate(query) ) {
  *          Long id = BoOrder.ID.of(order);
  *          String descr = BoOrder.DESCR.of(order);
  *          System.out.println(<span class="character">&quot;</span><span class="character">Order id: </span><span class="character">&quot;</span> + id + <span class="character">&quot;</span><span class="character"> descr: </span><span class="character">&quot;</span> + descr);
