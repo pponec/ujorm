5ef1cc4d9eac78978773aef5417851ef07080ad2
ponec <ponec@localhost>
2009-07-12 09:36:09 +0000

    Database support HSQLDB

--- ujo-core/src-orm/org/ujoframework/orm/sample/Database.java
+++ ujo-core/src-orm/org/ujoframework/orm/sample/Database.java
@@ -16,7 +16,6 @@
 
 package org.ujoframework.orm.sample;
 
-import org.ujoframework.orm.annot.Db;
 import org.ujoframework.orm.annot.Table;
 import org.ujoframework.implementation.orm.OrmTable;
 import org.ujoframework.implementation.orm.RelationToMany;
@@ -26,10 +25,12 @@ import org.ujoframework.orm.annot.View;
  * An table mapping to a database (a sample of usage).
  * @hidden
  */
-@Db(schema="db1", dialect=org.ujoframework.orm.dialect.H2Dialect.class, user="sa", password="", jdbcUrl="jdbc:h2:mem:db1")
+//@Db(schema="db1", dialect=org.ujoframework.orm.dialect.H2Dialect.class, user="sa", password="", jdbcUrl="jdbc:h2:mem:db1")
 //@Db(schema="db1", dialect=org.ujoframework.orm.dialect.PostgreSqlDialect.class, user="sa", password="sa", jdbcUrl="jdbc:postgresql://127.0.0.1:5432/db1")
 //@Db(schema="db1", dialect=org.ujoframework.orm.dialect.DerbyDialect.class, user="sa", password="", jdbcUrl="jdbc:derby:C:\\temp\\derby-sample;create=true")
 //@Db(schema="db1", dialect=org.ujoframework.orm.dialect.MySqlDialect.class, user="sa", password="sa", jdbcUrl="jdbc:mysql://127.0.0.1:3306/db1")
+//@Db(schema="db1", dialect=org.ujoframework.orm.dialect.HsqlDialect.class, user="sa", password="", jdbcUrl="jdbc:hsqldb:mem:db1")
+//@Db(schema="db1", dialect=org.ujoframework.orm.dialect.OracleDialect.class, user="sa", password="", jdbcUrl="jdbc:oracle:thin:@myhost:1521:orcl")
 public class Database extends OrmTable<Database> {
 
     /** Customer order. The used annotation overwrites a database schema from the property schema. */
