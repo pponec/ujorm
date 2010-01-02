eb6093af950c35b30bbf987f22994280a84ff8c8
Pavel Ponec <pponec@users.noreply.github.com>
2010-01-02 13:20:54 +0000

    MySqlDialect if fixed, explicit use of  the engine 'InnoDB' was implemented.

--- ujo-all-netbeans/src-orm/org/ujoframework/orm/dialect/MySqlDialect.java
+++ ujo-all-netbeans/src-orm/org/ujoframework/orm/dialect/MySqlDialect.java
@@ -19,9 +19,11 @@ package org.ujoframework.orm.dialect;
 import java.io.IOException;
 import org.ujoframework.orm.CriterionDecoder;
 import org.ujoframework.orm.SqlDialect;
+import org.ujoframework.orm.metaModel.MetaColumn;
+import org.ujoframework.orm.metaModel.MetaDatabase;
 import org.ujoframework.orm.metaModel.MetaTable;
 
-/** MySQL release 5.1
+/** Dialect for the MySQL since release 5.0 for the InnoDB engine.
  * <br><a href="http://dev.mysql.com/">http://dev.mysql.com/</a>
  */
 public class MySqlDialect extends SqlDialect {
@@ -44,16 +46,55 @@ public class MySqlDialect extends SqlDialect {
         , Appendable out
         ) throws IOException
     {
-        out.append("DELETE ");
-        out.append(table.getAlias());
-        out.append(" FROM ");
-        printTableAliasDefinition(table, out);
-        out.append(" WHERE ");
-        out.append(decoder.getWhere());
 
+        MetaTable[] tables = decoder.getTablesSorted(table);
+
+        if (tables.length>1) {
+            out.append("DELETE FROM ");
+            for (int i = 0; i < tables.length; i++) {
+                if (i > 0) { out.append(", "); }
+                printTableAliasDefinition(tables[i], out);
+            }
+            out.append(" WHERE ");
+            out.append(decoder.getWhere());
+            
+        } else {
+            String fullTableName = printFullTableName(table, new StringBuilder(64)).toString();
+            String where = decoder.getWhere().replace(table.getAlias()+'.', fullTableName+'.');
+            //
+            out.append("DELETE FROM ");
+            out.append(fullTableName);
+            out.append(" WHERE ");
+            out.append(where);
+        }
         return out;
     }
 
+    @Override
+    public Appendable printSequenceTable(MetaDatabase db, Appendable out) throws IOException {
+        return super.printSequenceTable(db, out).append(getEngine());
+    }
 
+    @Override
+    public Appendable printTable(MetaTable table, Appendable out) throws IOException {
+        return super.printTable(table, out).append(getEngine());
+    }
+
+    /** Returns a MySQL enginge. <br>
+     * The default value is " ENGINE = InnoDB". */
+    protected String getEngine() {
+        return " ENGINE = InnoDB";
+    }
+
+    /** MySQL dialect uses a database type DATETIME (instead of the TIMESTAMP) for the java.util.Date. */
+    @Override
+    protected String getColumnType(final MetaColumn column) {
+        switch (MetaColumn.DB_TYPE.of(column)) {
+            case TIMESTAMP:
+                return "DATETIME";
+            default:
+                return super.getColumnType(column);
+        }
+    }
 
 }
