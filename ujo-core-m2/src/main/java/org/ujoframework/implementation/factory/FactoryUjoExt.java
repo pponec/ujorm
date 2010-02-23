09ac599ec67bc8ca5ecf6a358da86e46d09895b0
Pavel Ponec <pponec@users.noreply.github.com>
2010-02-23 20:48:12 +0000

    Copy sources to the Maven project

---
+++ ujo-core-m2/src/main/java/org/ujoframework/implementation/factory/FactoryUjoExt.java
@@ -0,0 +1,40 @@
+/*
+ *  Copyright 2008-2010 Pavel Ponec
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.ujoframework.implementation.factory;
+
+import org.ujoframework.UjoProperty;
+import org.ujoframework.core.UjoManager;
+
+
+/**
+ * En extended <code>Ujo</code> implementation. The method does not implement all <code>UjoExt</code> method!
+ *
+ * @see FactoryProperty
+ * @author Pavel Ponec
+ * @since ujo-tool
+ * @composed 1 - * FactoryProperty
+ */
+abstract public class FactoryUjoExt<UJO_IMPL extends FactoryUjoExt> extends FactoryUjo {
+    
+    /** Getter based on one UjoProperty */
+    @SuppressWarnings("unchecked")
+    public <UJO extends UJO_IMPL, VALUE> VALUE get
+        ( UjoProperty<UJO, VALUE> property) {
+        return (VALUE) UjoManager.getValue(this, property);
+    }
+
+}    
