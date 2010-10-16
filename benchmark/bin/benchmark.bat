8a0b201f3364a710192676c9c1f9f058e2301d33
ponec <ponec@localhost>
2010-10-16 20:58:39 +0000

    Benchmark start script modification.

--- benchmark/bin/benchmark.bat
+++ benchmark/bin/benchmark.bat
@@ -1,34 +1,34 @@
 java -version  >> out.txt
 
 echo 00
+java -jar ..\ujo-orm\target\benchmark.ujorm-1.00.jar >> out.txt
 java -jar ..\hibernate\target\benchmark.hibernate.pojo-1.00.jar >> out.txt
 java -jar ..\hibernate-ujo\target\benchmark.hibernate.ujo-1.00.jar >> out.txt
-java -jar ..\ujo-orm\target\benchmark.ujorm-1.00.jar >> out.txt
 
 echo 01
+java -jar ..\ujo-orm\target\benchmark.ujorm-1.00.jar >> out.txt
 java -jar ..\hibernate\target\benchmark.hibernate.pojo-1.00.jar >> out.txt
 java -jar ..\hibernate-ujo\target\benchmark.hibernate.ujo-1.00.jar >> out.txt
-java -jar ..\ujo-orm\target\benchmark.ujorm-1.00.jar >> out.txt
 
 echo 02
+java -jar ..\ujo-orm\target\benchmark.ujorm-1.00.jar >> out.txt
 java -jar ..\hibernate\target\benchmark.hibernate.pojo-1.00.jar >> out.txt
 java -jar ..\hibernate-ujo\target\benchmark.hibernate.ujo-1.00.jar >> out.txt
-java -jar ..\ujo-orm\target\benchmark.ujorm-1.00.jar >> out.txt
 
 echo 03
+java -jar ..\ujo-orm\target\benchmark.ujorm-1.00.jar >> out.txt
 java -jar ..\hibernate\target\benchmark.hibernate.pojo-1.00.jar >> out.txt
 java -jar ..\hibernate-ujo\target\benchmark.hibernate.ujo-1.00.jar >> out.txt
-java -jar ..\ujo-orm\target\benchmark.ujorm-1.00.jar >> out.txt
 
 echo 04
+java -jar ..\ujo-orm\target\benchmark.ujorm-1.00.jar >> out.txt
 java -jar ..\hibernate\target\benchmark.hibernate.pojo-1.00.jar >> out.txt
 java -jar ..\hibernate-ujo\target\benchmark.hibernate.ujo-1.00.jar >> out.txt
-java -jar ..\ujo-orm\target\benchmark.ujorm-1.00.jar >> out.txt
 
 echo 05
+java -jar ..\ujo-orm\target\benchmark.ujorm-1.00.jar >> out.txt
 java -jar ..\hibernate\target\benchmark.hibernate.pojo-1.00.jar >> out.txt
 java -jar ..\hibernate-ujo\target\benchmark.hibernate.ujo-1.00.jar >> out.txt
-java -jar ..\ujo-orm\target\benchmark.ujorm-1.00.jar >> out.txt
 
 
 SHUTDOWN -s -t 01
