35613a33f54ada7eb4a614c05f40c5e31f024f2d
ponec <ponec@localhost>
2011-02-18 21:51:58 +0000

    JavaDoc

--- samples/generated/src/main/java/org/bo/gen/_Order.java
+++ samples/generated/src/main/java/org/bo/gen/_Order.java
@@ -14,6 +14,7 @@ import org.ujoframework.orm.DbType;
 import org.ujoframework.orm.annot.Column;
 import org.ujoframework.implementation.orm.OrmTable;
 import org.ujoframework.implementation.orm.RelationToMany;
+import org.ujoframework.orm.annot.Comment;
 import org.ujoframework.orm.annot.Table;
 
 @Table(name = "ord_order")
@@ -21,71 +22,86 @@ abstract public class _Order extends OrmTable<_Order> {
 
     /** Store the value like VARCHAR. */
     public enum State implements ValueExportable {
-
         ACTIVE,
         DELETED;
-
-        @Override
-        public String exportToString() {
+        @Override public String exportToString() {
             return name().substring(0, 1);
         }
     }
     /** The Unique Key */
+    @Comment("The Unique Key")
     @Column(pk = true)
     public static final UjoProperty<Order, Long> id = newProperty(Long.class);
     /** Order state, default is ACTIVE */
+    @Comment("Order state, default is ACTIVE")
     public static final UjoProperty<Order, State> state = newProperty(State.ACTIVE);
     /** User key */
+    @Comment("User key")
     public static final UjoProperty<Order, Integer> userId = newProperty(Integer.class);
     /** Description of the order */
+    @Comment("Description of the order")
     @Column(type = DbType.VARCHAR, name = "DESCR", mandatory = true)
     public static final UjoProperty<Order, String> descr = newProperty(String.class);
     /** Date of creation */
+    @Comment("Date of creation")
     public static final UjoProperty<Order, Date> created = newProperty(Date.class);
     /** Reference to Items */
+    @Comment("Reference to Items")
     public static final RelationToMany<Order, Item> items = newRelation(Item.class);
 
     // --- Getters / Setters ---
+
+    /** The Unique Key */
     public Long getId() {
         return id.getValue((Order) this);
     }
 
+    /** The Unique Key */
     public void setId(Long _id) {
         id.setValue((Order) this,_id);
     }
 
+    /** User key */
     public Integer getUsrId() {
         return userId.getValue((Order) this);
     }
 
+    /** User key */
     public void setUsrId(Integer _usrId) {
         userId.setValue((Order) this,_usrId);
     }
 
+    /** Description of the order */
     public String getDescr() {
         return descr.getValue((Order) this);
     }
 
+    /** Description of the order */
     public void setDescr(String _descr) {
         descr.setValue((Order) this,_descr);
     }
 
-    public Date getDate() {
+    /** Date of creation */
+    public Date getCreated() {
         return created.getValue((Order) this);
     }
 
-    public void setDate(Date _date) {
+    /** Date of creation */
+    public void setCreated(Date _date) {
         created.setValue((Order) this,_date);
     }
 
+    /** Order state, default is ACTIVE */
     public State getState() {
         return state.getValue((Order) this);
     }
 
+    /** Order state, default is ACTIVE */
     public void setState(State _state) {
         state.setValue((Order) this,_state);
     }
 
+    /** Reference to Items */
     public UjoIterator<Item> getItems() {
         return items.getValue((Order) this);
 
