f21086458383fb7db71e3aedaba357442d4cc3ef
meatfly <meatfly@localhost>
2011-04-11 22:35:09 +0000

    oprava rizeni autotranskakci v pripade rizeni pomoci @Transactional

--- project-m2/ujo-spring/src/main/java/org/ujoframework/orm/support/UjormTransactionManager.java
+++ project-m2/ujo-spring/src/main/java/org/ujoframework/orm/support/UjormTransactionManager.java
@@ -13,9 +13,9 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-
 package org.ujoframework.orm.support;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.springframework.transaction.TransactionDefinition;
@@ -34,49 +34,78 @@ public class UjormTransactionManager extends AbstractPlatformTransactionManager 
      * so we do not need to use transaction Objects
      */
     private UjoSessionFactory ujoSessionFactory;
+    //call stack @Transtactional deep
+    private AtomicInteger deep;
 
     @Override
     protected Object doGetTransaction() throws TransactionException {
         LOGGER.log(Level.INFO, "getTransaction returning new Object");
-        //    ujoSessionFactory.
         //can not return null because it will not call commit
         return new Object();
     }
 
     @Override
     protected void doBegin(Object o, TransactionDefinition td) throws TransactionException {
-        
-        LOGGER.log(Level.INFO, "Auto transaction registred/started");
-        ujoSessionFactory.setAutoTransaction(true);
+        if (incCalling()) {
+            LOGGER.log(Level.FINEST, "Auto transaction registred/started");
+            ujoSessionFactory.setAutoTransaction(true);
+        }
     }
 
     @Override
     protected void doCommit(DefaultTransactionStatus dts) throws TransactionException {
-
-
         dts.isGlobalRollbackOnly();
-        if (ujoSessionFactory.getDefaultSession().isRollbackOnly()) {
-            LOGGER.log(Level.WARNING, "Rolling back transaction becaouse has been mark as roll back only");
+        if (decCalling()) {
+            LOGGER.log(Level.FINEST, "Auto transaction ending (commit/rollback)");
 
-            rollback(dts);
-            return;
+            if (ujoSessionFactory.getDefaultSession().isRollbackOnly()) {
+                LOGGER.log(Level.WARNING, "Rolling back transaction becaouse has been mark as roll back only");
+                rollback(dts);
+                return;
+            }
+            LOGGER.log(Level.INFO, "commiting transaction ...");
+            ujoSessionFactory.getDefaultSession().commit();
+            ujoSessionFactory.setHasBeenrollbacked(false);
         }
-
-        LOGGER.log(Level.INFO, "commiting transaction ...");
-
-        ujoSessionFactory.getDefaultSession().commit();
-        ujoSessionFactory.setHasBeenrollbacked(false);
     }
 
     @Override
     protected void doRollback(DefaultTransactionStatus dts) throws TransactionException {
-        LOGGER.log(Level.WARNING, "roling back transaction");
+        LOGGER.log(Level.WARNING, "rolling back transaction");
 
         ujoSessionFactory.getDefaultSession().rollback();
         ujoSessionFactory.setHasBeenrollbacked(true);
+        deep = null;
     }
 
     public void setUjoSessionFactory(UjoSessionFactory ujoSessionFactory) {
         this.ujoSessionFactory = ujoSessionFactory;
+    }
+
+    /**
+     *
+     * @return true pokud je na zacatku deep 0 tedy jde o prvni vstup do sluzebni vrstvy
+     */
+    private boolean incCalling() {
+        if (deep == null) {
+            deep = new AtomicInteger(1);
+            return true;
+        } else {
+            deep.incrementAndGet();
+            return false;
+        }
+    }
+
+    /**
+     *
+     * @return true pokud je na konci deep 0 tedy o posledni vystup do sluzebni vrstvy
+     */
+    private boolean decCalling() {
+        if (deep.decrementAndGet() == 0) {
+            deep = null;
+            return true;
+        } else {
+            return false;
+        }
     }
 }
