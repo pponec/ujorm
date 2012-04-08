fb4ac18bab90ccf3aaa71f87fcee24f10b7010eb
ponec <ponec@localhost>
2012-04-08 08:38:14 +0000

    Benchmark: required Order count in the database can be assigned from a command line in the first parameter.

--- benchmark/bin/benchmark.bat
+++ benchmark/bin/benchmark.bat
@@ -1,36 +1,38 @@
 java -version  >> out.txt
 
+set ORDER_COUNT=2000
+
 echo 00
-java -jar ..\ujo-orm\target\benchmark.ujorm-1.00.jar >> out.txt
-java -jar ..\hibernate\target\benchmark.hibernate.pojo-1.00.jar >> out.txt
-java -jar ..\hibernate-ujo\target\benchmark.hibernate.ujo-1.00.jar >> out.txt
+java -jar ..\ujo-orm\target\benchmark.ujorm-1.23-SNAPSHOT.jar 10 >> out.txt
+java -jar ..\hibernate\target\benchmark.hibernate.pojo-1.23-SNAPSHOT.jar 10 >> out.txt
+java -jar ..\hibernate-ujo\target\benchmark.hibernate.ujo-1.23-SNAPSHOT.jar 10 >> out.txt
 
 echo 01
-java -jar ..\ujo-orm\target\benchmark.ujorm-1.00.jar >> out.txt
-java -jar ..\hibernate\target\benchmark.hibernate.pojo-1.00.jar >> out.txt
-java -jar ..\hibernate-ujo\target\benchmark.hibernate.ujo-1.00.jar >> out.txt
+java -jar ..\ujo-orm\target\benchmark.ujorm-1.23-SNAPSHOT.jar %ORDER_COUNT% >> out.txt
+java -jar ..\hibernate\target\benchmark.hibernate.pojo-1.23-SNAPSHOT.jar %ORDER_COUNT% >> out.txt
+java -jar ..\hibernate-ujo\target\benchmark.hibernate.ujo-1.23-SNAPSHOT.jar %ORDER_COUNT% >> out.txt
 
 echo 02
-java -jar ..\ujo-orm\target\benchmark.ujorm-1.00.jar >> out.txt
-java -jar ..\hibernate\target\benchmark.hibernate.pojo-1.00.jar >> out.txt
-java -jar ..\hibernate-ujo\target\benchmark.hibernate.ujo-1.00.jar >> out.txt
+java -jar ..\ujo-orm\target\benchmark.ujorm-1.23-SNAPSHOT.jar %ORDER_COUNT% >> out.txt
+java -jar ..\hibernate\target\benchmark.hibernate.pojo-1.23-SNAPSHOT.jar %ORDER_COUNT% >> out.txt
+java -jar ..\hibernate-ujo\target\benchmark.hibernate.ujo-1.23-SNAPSHOT.jar %ORDER_COUNT% >> out.txt
 
 echo 03
-java -jar ..\ujo-orm\target\benchmark.ujorm-1.00.jar >> out.txt
-java -jar ..\hibernate\target\benchmark.hibernate.pojo-1.00.jar >> out.txt
-java -jar ..\hibernate-ujo\target\benchmark.hibernate.ujo-1.00.jar >> out.txt
+java -jar ..\ujo-orm\target\benchmark.ujorm-1.23-SNAPSHOT.jar %ORDER_COUNT% >> out.txt
+java -jar ..\hibernate\target\benchmark.hibernate.pojo-1.23-SNAPSHOT.jar %ORDER_COUNT% >> out.txt
+java -jar ..\hibernate-ujo\target\benchmark.hibernate.ujo-1.23-SNAPSHOT.jar %ORDER_COUNT% >> out.txt
 
 echo 04
-java -jar ..\ujo-orm\target\benchmark.ujorm-1.00.jar >> out.txt
-java -jar ..\hibernate\target\benchmark.hibernate.pojo-1.00.jar >> out.txt
-java -jar ..\hibernate-ujo\target\benchmark.hibernate.ujo-1.00.jar >> out.txt
+java -jar ..\ujo-orm\target\benchmark.ujorm-1.23-SNAPSHOT.jar %ORDER_COUNT% >> out.txt
+java -jar ..\hibernate\target\benchmark.hibernate.pojo-1.23-SNAPSHOT.jar %ORDER_COUNT% >> out.txt
+java -jar ..\hibernate-ujo\target\benchmark.hibernate.ujo-1.23-SNAPSHOT.jar %ORDER_COUNT% >> out.txt
 
 echo 05
-java -jar ..\ujo-orm\target\benchmark.ujorm-1.00.jar >> out.txt
-java -jar ..\hibernate\target\benchmark.hibernate.pojo-1.00.jar >> out.txt
-java -jar ..\hibernate-ujo\target\benchmark.hibernate.ujo-1.00.jar >> out.txt
+java -jar ..\ujo-orm\target\benchmark.ujorm-1.23-SNAPSHOT.jar %ORDER_COUNT% >> out.txt
+java -jar ..\hibernate\target\benchmark.hibernate.pojo-1.23-SNAPSHOT.jar %ORDER_COUNT% >> out.txt
+java -jar ..\hibernate-ujo\target\benchmark.hibernate.ujo-1.23-SNAPSHOT.jar %ORDER_COUNT% >> out.txt
 
 
-SHUTDOWN -s -t 01
+echo SHUTDOWN -s -t 01
 
 
--- benchmark/hibernate/src/main/java/benchmark/BenchmarkHnP.java
+++ benchmark/hibernate/src/main/java/benchmark/BenchmarkHnP.java
@@ -13,7 +13,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-
 package benchmark;
 
 import java.math.BigDecimal;
@@ -34,22 +33,37 @@ import benchmark.bo.*;
  */
 public class BenchmarkHnP {
 
-    public static final int ORDER_COUNT = 2000;
-    public static final int ITEM_COUNT  = 7;
+    public static final String INFO = "** HIBERNATE " + "3.3.1.GA + POJO";
 
+    public static final int DEFAULT_ORDER_COUNT = 2000;
+    public static final int DEFAULT_ITEM_COUNT = 7;
+    public static final boolean DEFAULT_COMMIT_FLUSH_MODE = false;
+    //
+    private final int ORDER_COUNT;
+    private final int ITEM_COUNT;
+    private final boolean COMMIT_FLUSH_MODE;
+    //
     SessionFactory sessionFactory;
     Session session;
 
+    public BenchmarkHnP(int countOfOrder, int countOfItem, boolean commitFlushMode) {
+        this.ORDER_COUNT = countOfOrder;
+        this.ITEM_COUNT = countOfItem;
+        this.COMMIT_FLUSH_MODE = commitFlushMode;
+    }
+
     /** Before the first use you must load a meta-model. */
     public void loadMetaModel() {
-
-        System.out.println("** HIBERNATE " + "3.3.1.GA + POJO");
         Logger.getLogger("").setLevel(Level.SEVERE);
 
         long time1 = System.currentTimeMillis();
 
         sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
         session = sessionFactory.openSession();
+        if (this.COMMIT_FLUSH_MODE) {
+            // Note: the default mode of the Hibernate is "AUTO" due to reduced risks
+            session.setFlushMode(org.hibernate.FlushMode.COMMIT);
+        }
 
         printTime("META-DATA", time1, System.currentTimeMillis());
     }
@@ -63,20 +77,20 @@ public class BenchmarkHnP {
 
         HbmUser user1 = new HbmUser();
         user1.setLastname("Lorem ipsum dolor");
-        user1.setSurename( "Sit amet consectetur");
-        user1.setPersonalId( "12345678");
+        user1.setSurename("Sit amet consectetur");
+        user1.setPersonalId("12345678");
         session.save(user1);
 
         HbmUser user2 = new HbmUser();
-        user2.setLastname( "Lorem ipsum dolor");
-        user2.setSurename( "Sit amet consectetur");
-        user2.setPersonalId( "12345678");
+        user2.setLastname("Lorem ipsum dolor");
+        user2.setSurename("Sit amet consectetur");
+        user2.setPersonalId("12345678");
         session.save(user2);
 
-        for (int i=1; i<=ORDER_COUNT; i++) {
+        for (int i = 1; i <= ORDER_COUNT; i++) {
 
             HbmOrder order = new HbmOrder();
-            order.setDateOfOrder( new Date());
+            order.setDateOfOrder(new Date());
             order.setDeletionReason("NO");
             order.setDiscount(new BigDecimal(100));
             order.setLanguage("cs");
@@ -84,20 +98,20 @@ public class BenchmarkHnP {
             order.setPaid(true);
             order.setParent(null);
             order.setPaymentType("C");
-            order.setPublicId( "P"+String.valueOf(1001000+i));
+            order.setPublicId("P" + String.valueOf(1001000 + i));
             order.setUser(user1);
             session.save(order);
 
-            for (int j=1; j<=ITEM_COUNT; j++) {
-               HbmOrderItem item = new HbmOrderItem();
-               item.setArrival(false);
-               item.setCharge( new BigDecimal(1000-j));
-               item.setDescription( "Ut diam ante, aliquam ut varius at, fermentum non odio. Aliquam sodales, diam eu faucibus mattis");
-               item.setOrder( order);
-               item.setPrice( new BigDecimal(1000+j));
-               item.setPublicId( "xxss-"+j);
-               item.setUser( user2);
-               session.save(item);
+            for (int j = 1; j <= ITEM_COUNT; j++) {
+                HbmOrderItem item = new HbmOrderItem();
+                item.setArrival(false);
+                item.setCharge(new BigDecimal(1000 - j));
+                item.setDescription("Ut diam ante, aliquam ut varius at, fermentum non odio. Aliquam sodales, diam eu faucibus mattis");
+                item.setOrder(order);
+                item.setPrice(new BigDecimal(1000 + j));
+                item.setPublicId("xxss-" + j);
+                item.setUser(user2);
+                session.save(item);
             }
         }
 
@@ -105,7 +119,6 @@ public class BenchmarkHnP {
         printTime("INSERT", time1, System.currentTimeMillis());
 
     }
-
 
     /** Create database and using SELECT */
     @SuppressWarnings("unchecked")
@@ -130,9 +143,8 @@ public class BenchmarkHnP {
         }
 
         tr.commit();
-        printTime("SINGLE SELECT "+i, time1, System.currentTimeMillis());
+        printTime("SINGLE SELECT " + i, time1, System.currentTimeMillis());
     }
-
 
     /** Create database and using SELECT */
     @SuppressWarnings("unchecked")
@@ -142,7 +154,7 @@ public class BenchmarkHnP {
         Transaction tr = session.beginTransaction();
 
 
-        for (int i = -ORDER_COUNT; i<0 ; i++) {
+        for (int i = -ORDER_COUNT; i < 0; i++) {
             String hql = "from HbmOrder where id = :id and deleted = :deleted";
             Query query = session.createQuery(hql);
             query.setParameter("id", new Long(i));
@@ -151,10 +163,10 @@ public class BenchmarkHnP {
         }
 
         tr.commit();
-        printTime("EMPTY SELECT "+ORDER_COUNT, time1, System.currentTimeMillis());;
+        printTime("EMPTY SELECT " + ORDER_COUNT, time1, System.currentTimeMillis());
+        ;
 
     }
-
 
     /** Create database and using SELECT */
     @SuppressWarnings("unchecked")
@@ -171,7 +183,7 @@ public class BenchmarkHnP {
         int i = 0;
         for (HbmOrder order : orders) {
             String surename = order.getUser().getSurename();
-            if (false) System.out.println("Usr.surename: " + surename);
+            if (false) { System.out.println("Usr.surename: " + surename); }
 
             hql = "from HbmOrderItem where deleted = :deleted and order = :order";
             query = session.createQuery(hql);
@@ -181,20 +193,19 @@ public class BenchmarkHnP {
 
             for (HbmOrderItem item : items) {
                 ++i;
-                BigDecimal price  = item.getPrice();
+                BigDecimal price = item.getPrice();
                 BigDecimal charge = item.getCharge();
                 if (true) {
                     String lang = item.getOrder().getLanguage();
                     String name = item.getUser().getLastname();
-                    if (false) System.out.println(">>> Order.lang: " + lang + " User.lastname" + name);
+                    if (false) { System.out.println(">>> Order.lang: " + lang + " User.lastname" + name); }
                 }
             }
         }
 
         tr.commit();
-        printTime("MULTI SELECT "+i, time1, System.currentTimeMillis());
+        printTime("MULTI SELECT " + i, time1, System.currentTimeMillis());
     }
-
 
     /** Update a charge of the order items */
     @SuppressWarnings("unchecked")
@@ -216,7 +227,7 @@ public class BenchmarkHnP {
         }
 
         tr.commit();
-        printTime("UPDATE "+i, time1, System.currentTimeMillis());
+        printTime("UPDATE " + i, time1, System.currentTimeMillis());
     }
 
     /** Create database and using DELETE */
@@ -256,19 +267,18 @@ public class BenchmarkHnP {
         sessionFactory.close();
     }
 
-
-
     /** Print time message. */
     protected void printTime(String msg, long time1, long time2) {
-        long time = time2-time1;
-        double result = time/1000d;
-        System.out.println("TIME - "+msg + ": "+result);
+        long time = time2 - time1;
+        double result = time / 1000d;
+        System.out.println("TIME." + getClass().getSimpleName() + ": " + msg + ": " + result);
+
     }
 
     /** Test */
     public static void main(String[] args) {
         try {
-            BenchmarkHnP sample = new BenchmarkHnP();
+            BenchmarkHnP sample = newInstance(args);
 
             sample.loadMetaModel();
             sample.useInsert();
@@ -282,8 +292,33 @@ public class BenchmarkHnP {
         } catch (Throwable e) {
             e.printStackTrace();
         }
-
-        
     }
 
+    /**  Create new instance*/
+    public static BenchmarkHnP newInstance(String[] args) {
+        int i = -1;
+        try {
+            int countOfOrder = args.length > ++i ? Integer.parseInt(args[i]) : DEFAULT_ORDER_COUNT;
+            int countOfItem = /*args.length>++i ? Integer.parseInt(args[i]) :*/ DEFAULT_ITEM_COUNT;
+            boolean commitFlushMode = args.length > ++i ? "tyTY".contains(args[i].substring(0, 1)) : DEFAULT_COMMIT_FLUSH_MODE;
+
+            BenchmarkHnP result = new BenchmarkHnP(countOfOrder, countOfItem, commitFlushMode);
+            printInputParameters(result, args);
+            return result;
+        } catch (Throwable e) {
+            throw new RuntimeException("Usage: java -jar benchmark.jar [countOfOrder:int] [commitFlushMode:boolean]", e);
+        }
+    }
+
+    /** Print Input Parameters */
+    private static void printInputParameters(Object mainClas, Object[] params) {
+        StringBuilder sb = new StringBuilder(256);
+        sb.append(mainClas.getClass().getSimpleName());
+        sb.append(".java ");
+        for (Object par : params) {
+            sb.append(" ");
+            sb.append(par);
+        }
+        System.out.println(INFO + " (" + sb + ")");
+    }
 }
