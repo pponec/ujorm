1a091ee3da01a0137f1bcab1a8b8165139191f5b
Pavel Ponec <pponec@users.noreply.github.com>
2012-09-28 07:35:52 +0000

    The BASH script itself takes a version build .

--- build.sh
+++ build.sh
@@ -1,23 +1,30 @@
 #!/bin/sh
 # URL Tutorial: hhttps://docs.sonatype.org/display/Repository/Sonatype+OSS+Maven+Repository+Usage+Guide#SonatypeOSSMavenRepositoryUsageGuide-7a.DeploySnapshotsandStageReleaseswithMaven
 # URL Nexus: https://oss.sonatype.org/index.html#view-repositories;snapshots~browsestorage
+# SNAPSHOT deploy: mvn clean deploy -Pproduction -Psign -DskipTests
+###################################################################
 
-# Required Release:
-RELEASE=1.30.RC2
+# Required Release (example: RELEASE=1.30):
+RELEASE=$( cd project-m2/ujo-core; mvn help:evaluate -Dexpression=project.version | grep -v "\[" )
+echo RELEASE=${RELEASE}
+
+# Deploy URL:
+URL=https://oss.sonatype.org/service/local/staging/deploy/maven2/
 
 # Create the build:
 mvn clean install javadoc:jar source:jar -DskipTests -Pproduction
 cd "project-m2"
 
+# For all artefact SIGN and DEPLOY:
 for ARTEFACT in ujo-core ujo-orm ujo-spring 
 do
     (
-	echo $ARTEFACT
-	cd   $ARTEFACT/target
+	echo ARTEFACT=$ARTEFACT
+	cd $ARTEFACT/target
 
-	mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging -DpomFile=$ARTEFACT-$RELEASE.pom -Dfile=$ARTEFACT-$RELEASE.jar
-	mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging -DpomFile=$ARTEFACT-$RELEASE.pom -Dfile=$ARTEFACT-$RELEASE-sources.jar -Dclassifier=sources
-	mvn gpg:sign-and-deploy-file -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=sonatype-nexus-staging -DpomFile=$ARTEFACT-$RELEASE.pom -Dfile=$ARTEFACT-$RELEASE-javadoc.jar -Dclassifier=javadoc
+	mvn gpg:sign-and-deploy-file -Durl=${URL} -DrepositoryId=sonatype-nexus-staging -DpomFile=$ARTEFACT-$RELEASE.pom -Dfile=$ARTEFACT-$RELEASE.jar
+	mvn gpg:sign-and-deploy-file -Durl=${URL} -DrepositoryId=sonatype-nexus-staging -DpomFile=$ARTEFACT-$RELEASE.pom -Dfile=$ARTEFACT-$RELEASE-sources.jar -Dclassifier=sources
+	mvn gpg:sign-and-deploy-file -Durl=${URL} -DrepositoryId=sonatype-nexus-staging -DpomFile=$ARTEFACT-$RELEASE.pom -Dfile=$ARTEFACT-$RELEASE-javadoc.jar -Dclassifier=javadoc
     )
 done
 
