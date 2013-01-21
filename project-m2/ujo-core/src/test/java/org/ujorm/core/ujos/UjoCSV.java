843f27153d1ed3c12be182f68b0632fce811cf58
ponec <ponec@localhost>
2013-01-21 20:55:42 +0000

    Ujorm CSV documentation

--- project-m2/ujo-core/src/test/java/org/ujorm/core/ujos/UjoCSV.java
+++ project-m2/ujo-core/src/test/java/org/ujorm/core/ujos/UjoCSV.java
@@ -11,39 +11,39 @@ package org.ujorm.core.ujos;
 
 import org.ujorm.Key;
 import org.ujorm.core.KeyFactory;
-import org.ujorm.extensions.AbstractUjo;
+import org.ujorm.implementation.quick.QuickUjoMid;
 
 /**
  * UjoCSV
  * @author Pavel Ponec
  */
-public class UjoCSV extends AbstractUjo {
-    
+public class UjoCSV extends QuickUjoMid<UjoCSV> {
+
     private static final KeyFactory<UjoCSV> f = newFactory(UjoCSV.class);
 
     public static final Key<UjoCSV, String> P1 = f.newKey();
     public static final Key<UjoCSV, String> P2 = f.newKey();
     public static final Key<UjoCSV, String> P3 = f.newKey();
-    
+
     static {
         f.lock();
     }
-    
+
     // ---------------------------
-    
-    
+
+
     private boolean onZeroManager = true;
-    
+
     public boolean isOnZeroManager() {
         return onZeroManager;
     }
-    
+
     public void setOnZeroManager(boolean onZeroManager) {
         this.onZeroManager = onZeroManager;
     }
-    
+
     //public boolean readAuthorization(ACTION_ZERO_REPLACE, property, result, null) {}
-    
-   
-    
+
+
+
 }
