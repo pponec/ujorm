4ced293057f967c3030267ec1920019da658048a
Pavel Ponec <pponec@users.noreply.github.com>
2013-04-15 19:29:36 +0000

    UjormTransactionManager is fixed + jUnit test are done.

--- project-m2/ujo-spring/src/main/java/org/ujorm/spring/UjormTransactionManager.java
+++ project-m2/ujo-spring/src/main/java/org/ujorm/spring/UjormTransactionManager.java
@@ -79,6 +79,7 @@ public class UjormTransactionManager extends AbstractPlatformTransactionManager 
             }
         } finally {
             if (localSession.getTransaction() == null) {
+                localSession.close();
                 session.remove();
             }
         }
