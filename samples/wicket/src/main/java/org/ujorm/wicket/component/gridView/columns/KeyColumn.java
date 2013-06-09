a3f0cc5abfe3405cb8ca6594fe07535f8381b910
ponec <ponec@localhost>
2013-06-09 10:21:44 +0000

    Grid CSS styling

--- samples/wicket/src/main/java/org/ujorm/wicket/component/gridView/columns/KeyColumn.java
+++ samples/wicket/src/main/java/org/ujorm/wicket/component/gridView/columns/KeyColumn.java
@@ -13,8 +13,10 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.ujorm.wicket.component.gridView;
+package org.ujorm.wicket.component.gridView.columns;
 
+import org.apache.wicket.Component;
+import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.extensions.markup.html.repeater.data.grid.ICellPopulator;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.AbstractColumn;
 import org.apache.wicket.markup.html.basic.Label;
@@ -55,32 +57,32 @@ import org.ujorm.wicket.KeyModel;
  * The above will attach a label to the cell with a property model for the expression
  * &quot;name.first&quot;
  *
- * @see PropertyModel
+ * @see KeyModel
  *
  * @author Igor Vaynberg ( ivaynberg )
+ * @author Pavel Ponec
  * @param <UJO extends Ujo>
  *            The Model object type
  */
 public class KeyColumn<UJO extends Ujo, T> extends AbstractColumn<UJO, Key<UJO,T>> {
 
     private static final long serialVersionUID = 1L;
-    private final KeyRing<UJO> propertyExpression;
+    protected final KeyRing<UJO> keySerializable;
+    protected final String cssClass;
 
     /**
      * Creates a property column that is also sortable - the simplyfied methdod
      *
-     * @param displayModel
-     *            display model
-     * @param sortProperty
-     *            sort property
-     * @param property
-     *            wicket property expression used by PropertyModel
+     * @param displayModel display model
+     * @param sortProperty sort property
+     * @param key wicket property expression used by PropertyModel
+     * @param cssClass optional argument for a CSS class
      */
-    public KeyColumn(final Key<UJO,T> property) {
-        super(new Model(property.getName()));
-        this.propertyExpression = KeyRing.of(property);
+    public KeyColumn(final Key<UJO,T> key, String cssClass) {
+        super(new Model(key.getName()));
+        this.keySerializable = KeyRing.of(key);
+        this.cssClass = cssClass;
     }
-
 
     /**
      * Implementation of populateItem which adds a label to the cell whose model is the provided
@@ -89,20 +91,34 @@ public class KeyColumn<UJO extends Ujo, T> extends AbstractColumn<UJO, Key<UJO,T
      * @see ICellPopulator#populateItem(Item, String, IModel)
      */
     @Override
-    public void populateItem(final Item<ICellPopulator<UJO>> item, final String componentId,
-            final IModel<UJO> rowModel) {
-        item.add(new Label(componentId, createLabelModel(rowModel)));
+    public void populateItem(final Item<ICellPopulator<UJO>> item, final String componentId, final IModel<UJO> rowModel) {
+        final UJO ujo = rowModel.getObject();
+        final IModel<?> valueModel = createValueModel(ujo);
+        final Component value = createValueCoponent(componentId, valueModel);
+        appendCssClass(value, ujo);
+        item.add(value);
+    }
+
+    /** {@inheritDoc } */
+    @Override
+    public String getCssClass() {
+        return cssClass;
+    }
+
+    /** Create the Label for a Value component */
+    private Component createValueCoponent(final String componentId, final IModel<?> valueModel) {
+        return new Label(componentId, valueModel);
     }
 
     /**
      * Factory method for generating a model that will generated the displayed value. Typically the
-     * model is a property model using the {@link #propertyExpression} specified in the constructor.
+     * model is a property model using the {@link #keySerializable} specified in the constructor.
      *
      * @param rowModel
      * @return model
      */
-    protected IModel<UJO> createLabelModel(final IModel<UJO> rowModel) {
-        final IModel result = KeyModel.of(rowModel, propertyExpression);
+    protected IModel<?> createValueModel(final UJO ujo) {
+        final IModel result = KeyModel.of(ujo, keySerializable.getFirstKey());
         return result;
     }
 
@@ -110,11 +126,25 @@ public class KeyColumn<UJO extends Ujo, T> extends AbstractColumn<UJO, Key<UJO,T
      * @return wicket property expression
      */
     public String getPropertyExpression() {
-        return propertyExpression.toString();
+        return keySerializable.toString();
     }
 
     /** A factory method */
     public static <U extends Ujo, T> KeyColumn<U,T> of(Key<U,T> key) {
-        return new KeyColumn(key);
+        return new KeyColumn(key, null);
     }
+
+    /** A factory method */
+    public static <U extends Ujo, T> KeyColumn<U,T> of(Key<U,T> key, String cssClass) {
+        return new KeyColumn(key, cssClass);
+    }
+
+    /** Append CSS class */
+    protected void appendCssClass(final Component value, final UJO ujo) {
+        final String cssClass$ = getCssClass();
+        if (cssClass$ != null) {
+            value.add(new AttributeAppender("class", new Model(cssClass$), " "));
+        }
+    }
+
 }
