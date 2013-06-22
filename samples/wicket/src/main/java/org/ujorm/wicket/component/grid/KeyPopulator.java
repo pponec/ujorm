ab7a869ae6b87893889bada0c3eb643f473ca348
Pavel Ponec <pponec@users.noreply.github.com>
2013-06-22 19:44:40 +0000

    Refactoring of the module ujo-wicket.

--- samples/wicket/src/main/java/org/ujorm/wicket/component/grid/KeyPopulator.java
+++ samples/wicket/src/main/java/org/ujorm/wicket/component/grid/KeyPopulator.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.ujorm.wicket.component.gridView;
+package org.ujorm.wicket.component.grid;
 
 import java.util.ArrayList;
 import java.util.List;
