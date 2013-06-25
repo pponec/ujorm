830f2446377a584fe3879f79bd65ebffea00896c
Pavel Ponec <pponec@users.noreply.github.com>
2013-06-25 07:15:42 +0000

    Hotel edit dialog

---
+++ samples/wicket/src/main/java/org/ujorm/hotels/gui/hotel/HotelEditor.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2013, Pavel Ponec
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ujorm.hotels.gui.hotel;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.repeater.RepeatingView;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.ujorm.Key;
+import org.ujorm.hotels.entity.City;
+import org.ujorm.hotels.entity.Hotel;
+import org.ujorm.wicket.component.edit.Field;
+import static org.ujorm.wicket.component.edit.UjoAbstractValidator.*;
+
+/**
+ * Hotel Editor
+ * @author Pavel Ponec
+ */
+public class HotelEditor extends Panel {
+   private static final long serialVersionUID = 20130621L;
+
+   private final Form<?> form;
+   private final ModalWindow modalWindow;
+   private Field repeatUntilDate;
+   private Field period;
+   private RepeatingView repeater;
+
+   public HotelEditor(ModalWindow modalWindow, IModel<Hotel> model) {
+        super(modalWindow.getContentId(), model);
+        this.modalWindow = modalWindow;
+        this.setOutputMarkupId(true);
+        this.setOutputMarkupPlaceholderTag(true);
+
+        // Form:
+        this.add(form = new Form("hotelForm"));
+        form.setOutputMarkupId(true);
+        form.add(createSaveButton("saveButton", "Save"));
+        form.add(createCancelButton("cancelButton", "Cancel"));
+
+        // Fields:
+        form.addOrReplace(repeater = new RepeatingView("fieldRepeater"));
+
+        // Edittable fields:
+        repeater.add(new Field(Hotel.NAME, getHotelModel()).setValidator(getMandatoryValidator()));
+        repeater.add(new Field(Hotel.CITY.add(City.NAME), getHotelModel()));
+        repeater.add(new Field(Hotel.STREET, getHotelModel()));
+        repeater.add(new Field(Hotel.PHONE, getHotelModel()));
+        repeater.add(new Field(Hotel.STARS, getHotelModel()));
+        repeater.add(new Field(Hotel.PRICE, getHotelModel()));
+        repeater.add(new Field(Hotel.CURRENCY, getHotelModel()));
+        repeater.add(new Field(Hotel.NOTE, getHotelModel()).setValidator(getMandatoryValidator()));
+        repeater.add(new Field(Hotel.ACTIVE, getHotelModel()));
+
+        modalWindow.setContent(this);
+    }
+
+    /** Returns a current entity */
+    final public Hotel getHotel() {
+        return (Hotel) getDefaultModelObject();
+    }
+
+    /** Returns a current entity model */
+    @SuppressWarnings("unchecked")
+    private IModel<Hotel> getHotelModel() {
+        return (IModel<Hotel>) getDefaultModel();
+    }
+
+    /** Vytvoří textfield pro aktuání model */
+    private Component createSaveButton(String id, String name) {
+        final AjaxButton result = new AjaxButton(id, Model.of(name), form) {
+
+            @Override
+            protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+                target.add(form);
+                modalWindow.close(target);
+            }
+
+            @Override
+            protected void onError(AjaxRequestTarget target, Form<?> form) {
+                target.add(form);
+            }
+        };
+        return result;
+    }
+
+    /** Vytvoří textfield pro aktuání model */
+    private Component createCancelButton(String id, String name) {
+        final AjaxButton result = new AjaxButton(id, Model.of(name), form) {
+
+            @Override
+            protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+                target.add(form);
+                modalWindow.close(target);
+            }
+
+            @Override
+            protected void onError(AjaxRequestTarget target, Form<?> form) {                
+                modalWindow.close(target);
+            }
+        };
+        return result;
+    }
+
+    /** Kopíruje všechny atributy z parametru */
+    public void showDialog(Hotel hotel, AjaxRequestTarget target, String title) {
+        this.repeatUntilDate.setVisible(hotel.ID.of(hotel) == null);
+        //
+        Hotel h = getHotel();
+        for (Key k : h.readKeys()) {
+            k.copy(h, hotel);
+        }
+
+        getModalWindow().setTitle(title);
+        getModalWindow().show(target);
+        target.add(form);
+    }
+
+    /** Returns modal WIndow */
+    public ModalWindow getModalWindow() {
+        return modalWindow;
+    }
+
+    /** Clear input */
+    public void clearInput() {
+        form.clearInput();
+    }
+
+}
---
+++ samples/wicket/src/main/java/org/ujorm/wicket/UjoEvent.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2013 Pavel Ponec
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ujorm.wicket;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.ujorm.Ujo;
+
+/**
+ * UjoEvent
+ * @see http://www.wicket-library.com/wicket-examples/events/wicket/bookmarkable/org.apache.wicket.examples.source.SourcesPage?0&SourcesPage_class=org.apache.wicket.examples.events.IndexPage&source=DecoupledAjaxUpdatePage.java
+ * @see http://savicprvoslav.blogspot.cz/2012/06/wicket-15-inter-component-events.html
+ * @see http://balamaci.wordpress.com/2011/04/19/wicket-1-5-intercomponent-comunication/
+ * @see http://wickeria.com/blog/12-05-23-par-tipu-pro-praci-s-apache-wicket
+ * @author Pavel Ponec
+ */
+public class UjoEvent {
+
+    public static final String CREATE = "CREATE";
+    public static final String READ = "DISPLAY";
+    public static final String UPDATE = "UPDATE";
+    public static final String DELETE = "DELETE";
+    public static final String CLONE = "COPY";
+    public static final String EXIT = "EXIT";
+
+
+    private Ujo ujo;
+    private AjaxRequestTarget target;
+    private String context;
+
+    public UjoEvent(String context, Ujo ujo, AjaxRequestTarget target) {
+        this.context = context;
+        this.ujo = ujo;
+        this.target = target;
+    }
+
+    public Ujo getUjo() {
+        return ujo;
+    }
+
+    public AjaxRequestTarget getTarget() {
+        return target;
+    }
+
+    public String getContext() {
+        return context;
+    }
+
+}
