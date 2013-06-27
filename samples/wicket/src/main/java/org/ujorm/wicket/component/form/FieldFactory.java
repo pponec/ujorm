563ebe5f34ef0b885df007e544e0965c545007df
Pavel Ponec <pponec@users.noreply.github.com>
2013-06-27 17:15:50 +0000

    Ujorm validator support

--- samples/wicket/src/main/java/org/ujorm/wicket/component/form/FieldFactory.java
+++ samples/wicket/src/main/java/org/ujorm/wicket/component/form/FieldFactory.java
@@ -19,9 +19,17 @@ import java.io.Serializable;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Set;
+import org.apache.wicket.Application;
+import org.apache.wicket.Component;
+import org.apache.wicket.markup.html.form.FormComponent;
 import org.apache.wicket.markup.repeater.RepeatingView;
 import org.ujorm.Key;
 import org.ujorm.Ujo;
+import org.ujorm.Validator;
+import org.ujorm.orm.OrmHandler;
+import org.ujorm.orm.OrmHandlerProvider;
+import org.ujorm.orm.metaModel.MetaColumn;
+import org.ujorm.validator.ValidatorUtils;
 
 /**
  * Field Factory
@@ -32,17 +40,24 @@ public class FieldFactory<U extends Ujo> implements Serializable {
     private RepeatingView repeatingView;
     private HashMap<String, Field> fields = new HashMap<String, Field>(16);
 
+    transient private OrmHandler ormHandler;
+
+    public FieldFactory(String repeatingViewId) {
+        this(new RepeatingView(repeatingViewId));
+    }
+
     public FieldFactory(RepeatingView repeatingView) {
         this.repeatingView = repeatingView;
     }
 
     /** Add new field to a repeating view*/
-    public void add(Key key, Field field) {
+    public void add(final Key key, final Field field) {
         Field oldField = fields.put(key.getName(), field);
         if (oldField != null) {
             throw new IllegalStateException("Field is assigned for the key: " + field);
         }
         repeatingView.add(field);
+        assignValidator(key, field);
     }
 
     /** Add new field to a repeating view*/
@@ -70,6 +85,48 @@ public class FieldFactory<U extends Ujo> implements Serializable {
     /** Return all keys in a String format */
     public Set<String> getKeyNames() {
         return fields.keySet();
+    }
+
+    /** Returns the repeating view */
+    public RepeatingView getRepeatingView() {
+        return repeatingView;
+    }
+
+    /** Returns OrmHandler */
+    protected OrmHandler getOrmHandler() {
+        if (ormHandler != null) {
+            Application appl = repeatingView.getPage().getApplication();
+            if (appl instanceof OrmHandlerProvider) {
+                ormHandler = ((OrmHandlerProvider) appl).getOrmHandler();
+            }
+        }
+
+        return ormHandler;
+    }
+
+    /** Is the key mandatory ? */
+    protected boolean isMandatory(Key key) {
+        final OrmHandler handler = getOrmHandler();
+        if (handler != null) {
+            MetaColumn column = handler.findColumnModel(key, false);
+            if (column.isMandatory()) {
+                return true;
+            }
+        }
+        return ValidatorUtils.isMandatoryValidator(key.getValidator());
+    }
+
+    /** Assign the validator */
+    protected void assignValidator(final Key key, final Field field) {
+        final Validator validator = key.getValidator();
+        if (validator != null) {
+            field.setValidator(new UjoValidator(validator, key));
+        } else if (isMandatory(key)) {
+            Component input = field.getInput();
+            if (input instanceof FormComponent) {
+                ((FormComponent)input).setRequired(true);
+            }
+        }
     }
 
 }
