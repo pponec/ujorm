0b72cd3aeb2c6c587c6deff41aaf183669989519
Pavel Ponec <pponec@users.noreply.github.com>
2013-07-10 17:40:37 +0000

    New class the WicketModel factory

---
+++ project-m2/ujo-wicket/src/main/java/org/ujorm/wicket/ModelFactory.java
@@ -0,0 +1,40 @@
+/*
+ *  Copyright 2013-2013 Pavel Ponec
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.ujorm.wicket;
+
+import org.apache.wicket.model.IModel;
+import org.ujorm.Key;
+import org.ujorm.Ujo;
+
+public class ModelFactory<U extends Ujo> {
+
+    /** Object model */
+    private final U domain;
+
+    /**
+     * Constructor
+     * @param domain A domain bean
+     */
+    public ModelFactory(U domain) {
+        this.domain = domain;
+    }
+
+    /** Create new model for the key */
+    public <T> IModel<T> getModel(Key<U,T> key) {
+        return new KeyModel(domain, key);
+    }
+
+}
