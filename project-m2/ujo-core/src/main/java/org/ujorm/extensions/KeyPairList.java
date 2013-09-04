791545e3c58720bdc623228f54435e4ff7b22587
ponec <ponec@localhost>
2013-09-04 17:08:24 +0000

    Core: the new class KeyPairList to copy between Ujo objects

---
+++ project-m2/ujo-core/src/main/java/org/ujorm/extensions/KeyPairList.java
@@ -0,0 +1,106 @@
+/*
+ *  Copyright 2007-2013 Pavel Ponec
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.ujorm.extensions;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.ujorm.CompositeKey;
+import org.ujorm.Key;
+import org.ujorm.Ujo;
+
+/**
+ * The help class to simple copy between Ujo objects.
+ * @author Pavel Ponec
+ */
+public class KeyPairList<SRC extends Ujo, TRG extends Ujo> {
+
+    private List<PairItem> pairs = new ArrayList<PairItem>();
+    /** Locked sign */
+    private boolean locked;
+
+    public <V> void add(Key<SRC, V> source, Key<TRG, V> target) throws UnsupportedOperationException {
+        checkLock();
+        pairs.add(new PairItem(source, target));
+    }
+
+    private void checkLock() throws UnsupportedOperationException {
+        if (locked) {
+            throw new UnsupportedOperationException("The class is locked");
+        }
+    }
+
+    public KeyPairList<SRC, TRG> lock() {
+        locked = true;
+        return this;
+    }
+
+    public void copyToTarget(SRC src, TRG target) {
+        for (PairItem pairItem : pairs) {
+            pairItem.copyToTarget(src, target);
+        }
+    }
+
+    public void copyToSource(TRG target, SRC src) {
+        for (PairItem pairItem : pairs) {
+            pairItem.copyToSource(target, src);
+        }
+    }
+
+    // ------------- PAIR CLASS -------------
+
+    private static final class PairItem<SRC extends Ujo, TRG extends Ujo, V> {
+
+        private final Key<SRC, V> srcKey;
+        private final Key<TRG, V> trgKey;
+        private final boolean compositeSrc;
+        private final boolean compositeTrg;
+
+        public PairItem(Key<SRC, V> srcKey, Key<TRG, V> tgtKey) {
+            this.srcKey = srcKey;
+            this.trgKey = tgtKey;
+            this.compositeSrc = srcKey.isComposite();
+            this.compositeTrg = tgtKey.isComposite();
+        }
+
+        /** Copy value to target */
+        private void copyToTarget(SRC source, TRG target) {
+            final Object value = srcKey.of(source);
+            if (compositeTrg) {
+                ((CompositeKey) trgKey).setValue(target, value, true);
+            } else {
+                trgKey.setValue(target, (V) value);
+            }
+        }
+
+        /** Copy value to source */
+        private void copyToSource(TRG target, SRC source) {
+            final Object value = trgKey.of(target);
+            if (compositeSrc) {
+                ((CompositeKey) srcKey).setValue(source, value, true);
+            } else {
+                srcKey.setValue(source, (V) value);
+            }
+        }
+    }
+
+    // ----------- FACTORY -----------
+
+    public static <SRC extends Ujo, TRG extends Ujo, V> KeyPairList<SRC,TRG> get() {
+        return new KeyPairList<SRC, TRG>();
+    }
+
+
+}
---
+++ project-m2/ujo-core/src/test/java/org/ujorm/extensions/KeyPairListTest.java
@@ -0,0 +1,64 @@
+/*
+ *  Copyright 2007-2013 Pavel Ponec
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.ujorm.extensions;
+
+import junit.framework.TestCase;
+import org.ujorm.implementation.universe.UniUjoBase;
+
+/**
+ * KeyPairListTest
+ * @author Pavel Ponec
+ */
+public class KeyPairListTest extends TestCase {
+
+    public KeyPairListTest(String testName) {
+        super(testName);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of copyToTarget method, of class KeyPairList.
+     */
+    public void testCopyToTarget() {
+        System.out.println("copyToTarget");
+
+        UniUjoBase p1 = new UniUjoBase();
+        UniUjoBase p2 = new UniUjoBase();
+
+        KeyPairList<UniUjoBase,UniUjoBase> pairs = KeyPairList.get();
+
+        pairs.add(UniUjoBase.PRO_P0, UniUjoBase.PRO_P0);
+        pairs.add(UniUjoBase.PRO_P1, UniUjoBase.PRO_P1);
+        pairs.add(UniUjoBase.PRO_P2, UniUjoBase.PRO_P2);
+
+        UniUjoBase.PRO_P0.setValue(p1, 300L);
+        UniUjoBase.PRO_P1.setValue(p1, 9);
+        pairs.copyToSource(p1, p2);
+        assertEquals(UniUjoBase.PRO_P0.of(p1), UniUjoBase.PRO_P0.of(p2));
+        assertEquals(UniUjoBase.PRO_P1.of(p1), UniUjoBase.PRO_P1.of(p2));
+    }
+
+
+}
