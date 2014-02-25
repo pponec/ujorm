25e5ce95939813dad9804c04320914415d7c1063
Pavel Ponec <pponec@users.noreply.github.com>
2014-02-25 19:22:58 +0000

    The calendar year is updated in header comments

--- project-m2/ujo-orm/src/main/java/org/ujorm/orm/metaModel/MetaDbServiceEx.java
+++ project-m2/ujo-orm/src/main/java/org/ujorm/orm/metaModel/MetaDbServiceEx.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2013-2013 Effectiva Solutions company
+ *  Copyright 2013-2014 Effectiva Solutions company
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
