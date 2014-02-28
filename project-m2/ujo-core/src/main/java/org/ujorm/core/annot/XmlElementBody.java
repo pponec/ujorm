d142ee9a679adf240cb7eb98d94486613d39943b
ponec <ponec@localhost>
2014-02-28 19:46:10 +0000

    Annotation headers are fixed

--- project-m2/ujo-core/src/main/java/org/ujorm/core/annot/XmlElementBody.java
+++ project-m2/ujo-core/src/main/java/org/ujorm/core/annot/XmlElementBody.java
@@ -1,16 +1,23 @@
 /*
- * XmlAttribute.java
+ *  Copyright 2008-2014 Pavel Ponec
  *
- * Created on 13. duben 2008, 11:55
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 package org.ujorm.core.annot;
 import java.lang.annotation.*;
 
-/** 
+/**
  * The annotation select an property containing a <strong>body of the element</strong>.
  * There is recommended that only one property was signed by the annoataton in the class.
  * If more annotated keys are identified, than the framework will be considered the valid property with the highest index.
@@ -19,5 +26,5 @@ import java.lang.annotation.*;
 @Retention(value=RetentionPolicy.RUNTIME)
 @Target(value=ElementType.FIELD)
 public @interface XmlElementBody {
-    
+
 }
