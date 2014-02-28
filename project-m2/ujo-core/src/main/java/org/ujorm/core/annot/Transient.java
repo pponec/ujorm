80759edb221485a364ccb62dff184295e80454e1
Pavel Ponec <pponec@users.noreply.github.com>
2014-02-28 19:46:10 +0000

    Annotation headers are fixed

--- project-m2/ujo-core/src/main/java/org/ujorm/core/annot/Transient.java
+++ project-m2/ujo-core/src/main/java/org/ujorm/core/annot/Transient.java
@@ -1,21 +1,28 @@
 /*
- * XmlAttribute.java
+ *  Copyright 2008-2014 Pavel Ponec
  *
- * Created on 13. duben 2008, 11:55
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 package org.ujorm.core.annot;
 import java.lang.annotation.*;
 
-/** 
+/**
  * Use the annotation to mark a Key static field to a transient state.
  * Values of the transient keys are not stored by any way.
  */
 @Retention(value=RetentionPolicy.RUNTIME)
 @Target(value=ElementType.FIELD)
 public @interface Transient {
-    
+
 }
