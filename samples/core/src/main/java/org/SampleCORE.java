6f5b21dfccb74f6c06188796550c5a0e7c80055e
Pavel Ponec <pponec@users.noreply.github.com>
2014-04-25 21:37:23 +0000

    The SampleCORE tutorial is updated

--- samples/core/src/main/java/org/SampleCORE.java
+++ samples/core/src/main/java/org/SampleCORE.java
@@ -140,7 +140,8 @@ public class SampleCORE {
         for (Key key : employee.readKeys()) {
             employee.set(key, null);
         }
-
+        
+        assert employee.getWage() == 0 : "Default value is zero";
         assert employee.getWage() == WAGE.getDefault() : "Check the default value";
     }
 
