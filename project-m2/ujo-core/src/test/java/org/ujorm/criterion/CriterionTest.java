1636e798773be2f859e692738c4cb1bd41ef48be
Pavel Ponec <pponec@users.noreply.github.com>
2014-06-10 20:51:41 +0000

    CORE: the method Criterion.cast() for the case where the current Key domain is from a parent class.

--- project-m2/ujo-core/src/test/java/org/ujorm/criterion/CriterionTest.java
+++ project-m2/ujo-core/src/test/java/org/ujorm/criterion/CriterionTest.java
@@ -178,7 +178,7 @@ public class CriterionTest extends MyTestCase {
         final Criterion<User> crn1, crn2, crn3;
 
         crn1 = User.LOGIN.whereEq("myLogin");
-        crn2 = User.NAME.whereEq("Pavel").retype();
+        crn2 = User.NAME.whereEq("Pavel").cast();
         crn3 = crn2.and(crn1);
 
         expected = "User(login EQ \"myLogin\")";
