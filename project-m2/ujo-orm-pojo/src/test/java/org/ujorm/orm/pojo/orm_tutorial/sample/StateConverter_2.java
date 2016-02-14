35ef680eed301e8b8f5cb6e7b967544d480a1116
ponec <ponec@localhost>
2016-02-14 13:53:55 +0100

    ORM2POHO: Sample cleaning

---
+++ project-m2/ujo-orm-pojo/src/test/java/org/ujorm/orm/pojo/orm_tutorial/sample/StateConverter_2.java
@@ -0,0 +1,53 @@
+/*
+ *  Copyright 2012 pavel.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package org.ujorm.orm.pojo.orm_tutorial.sample;
+
+import org.ujorm.orm.pojo.orm_tutorial.sample.entity.Order;
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.ujorm.orm.ITypeService;
+import org.ujorm.orm.metaModel.MetaColumn;
+import org.ujorm.orm.pojo.orm_tutorial.sample.entity.Order.State;
+
+/**
+ *
+ * @author pavel
+ */
+public class StateConverter_2 implements ITypeService<Order.State, Short> {
+
+    public State getValue(MetaColumn mColumn, ResultSet rs, int c) throws SQLException {
+        int i = rs.getInt(c);
+        return State.values()[i];
+    }
+
+    public State getValue(MetaColumn mColumn, CallableStatement rs, int c) throws SQLException {
+        int i = rs.getInt(c);
+        return State.values()[i];
+    }
+
+    public void setValue(MetaColumn mColumn, PreparedStatement rs, State value, int c) throws SQLException {
+        rs.setInt(c, value.ordinal());
+    }
+
+    public Class<Short> getDbTypeClass(MetaColumn column) {
+        return Short.class;
+    }
+
+}
