6de16c345e2a071e71cdda14f6bd9f2a25352cd0
Pavel Ponec <ponec@gmail.com>
2016-03-02 20:46:44 +0100

    HOTELS: buildinfo

--- samples/wicket/src/main/java/org/ujorm/hotels/gui/HomePage.java
+++ samples/wicket/src/main/java/org/ujorm/hotels/gui/HomePage.java
@@ -30,7 +30,7 @@ import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.ujorm.hotels.entity.Customer;
 import org.ujorm.hotels.gui.about.AboutPanel;
-import org.ujorm.hotels.gui.about.DeployInfo;
+import org.ujorm.hotels.gui.about.BuildInfo;
 import org.ujorm.hotels.gui.about.MeasuringCode;
 import org.ujorm.hotels.gui.booking.BookingTable;
 import org.ujorm.hotels.gui.customer.CustomerTable;
@@ -81,7 +81,7 @@ public class HomePage extends WebPage implements IAjaxIndicatorAware {
         Label label;
         add(waitingIcon = new WaitingIcon("waitingIcon"));
         add(new MeasuringCode("measuringCode"));
-        add(new DeployInfo("deployInfo"));
+        add(new BuildInfo("buildInfo"));
         add(new Label("applicationTitle", MainApplication.APPLICATION_NAME));
         add(label = new Label("applicationHeader", MainApplication.APPLICATION_NAME));
         if (!applParams.isProduction()) {
