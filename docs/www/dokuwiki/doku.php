d02c761f501c127afcc72f6ca7c7ea63d8deb12d
Pavel Ponec <ponec@gmail.com>
2016-03-07 21:57:13 +0100

    CORE: Dokuwiki backup

---
+++ docs/www/dokuwiki/doku.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html dir="ltr" class="js desktop" lang="en">
+<head>
+<meta http-equiv="refresh" content="0; url=index.html" />
+</head>
+<body>
+<a href="index.html">Redirect to <strong>Dokuwiki</strong> home page</a>
+</body>
+</html>
