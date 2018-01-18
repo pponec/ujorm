8aa1f8a3205982602cd84ee163e8b1235ed9cabd
pavel <pavel@X555LA>
2018-01-18 20:29:48 +0100

    CORE: code cleaning

--- project-m2/ujo-core/src/test/java/org/ujorm/implementation/xml/XmlUjoRoot_1.java
+++ project-m2/ujo-core/src/test/java/org/ujorm/implementation/xml/XmlUjoRoot_1.java
@@ -19,11 +19,11 @@ import org.ujorm.implementation.map.MapUjo;
  * @author Pavel Ponec
  */
 public class XmlUjoRoot_1 extends MapUjo  {
-    
+
     public static final Key<XmlUjoRoot_1,Long>       PRO_P0 = newKey("P0");
     public static final Key<XmlUjoRoot_1,XmlUjoItem> PRO_P1 = newKey("P1");
     public static final Key<XmlUjoRoot_1,String>     PRO_P2 = newKey("P2");
-    public static final Key<XmlUjoRoot_1, Ujo>       PRO_P3 = newKey("P3");
+    public static final Key<XmlUjoRoot_1,Ujo>        PRO_P3 = newKey("P3");
     public static final Key<XmlUjoRoot_1,Object[]>   PRO_P4 = newKey("ARRAY_OBJ");
     public static final Key<XmlUjoRoot_1,ArrayList>  PRO_P5 = newKey("LIST");
 
