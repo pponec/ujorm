a3b9b8061f2c6bbe915d836ade3d18b6b1f44b68
Pavel Ponec <pponec@gmail.com>
2019-04-24 15:10:43 +0200

    ALL: Building the "ujo-web" module with dependences

---
+++ buildWeb.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Building the "ujo-web" module with dependences
+################################################
+
+alias mvn='/opt/maven/default/bin/mvn'
+
+# Create the build:
+cd "$(dirname $0)/project-m2"
+
+cd project-m2; 
+mvn -DskipTests=true --also-make --projects ujo-web install
+
+echo "Finished"
+
+
