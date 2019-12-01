6617e93260e7fa61140b230fb3460b910f6d5a0e
Pavel Ponec <pponec@gmail.com>
2019-12-01 20:20:06 +0100

    TOOLS: MultiRun for supporting many tasks in threads (tests)

--- project-m2/ujo-tools/src/main/java/org/ujorm/tools/thread/MultiRun.java
+++ project-m2/ujo-tools/src/main/java/org/ujorm/tools/thread/MultiRun.java
@@ -17,8 +17,6 @@
 package org.ujorm.tools.thread;
 
 import java.time.Duration;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -26,7 +24,6 @@ import java.util.concurrent.TimeoutException;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 import javax.annotation.Nonnull;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
@@ -36,32 +33,59 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
  */
 public class MultiRun<P> {
 
+    /** Job arguments */
     private final Stream<P> params;
 
     public MultiRun(@Nonnull final Stream<P> params) {
         this.params = params;
     }
 
-    /** Get result as a Stream */
+    /** Get result as a Stream
+     * @param timeout The maximum time to wait
+     * @param job Job with a stream result
+     * @return The result stream
+     * */
     public <R> Stream<R> getStream(@Nonnull final Duration timeout, @Nonnull final Function<P, Stream<R>> job)
             throws MultiRunException {
         return params.map(params -> CompletableFuture.supplyAsync(() -> job.apply(params)))
                 .collect(Collectors.toList()).stream() // For a parallel processing!
-                .map(createFunction(timeout))
+                .map(createGrabber(timeout))
                 .flatMap(Function.identity()); // Join all streams
     }
 
-    /** Get a single value result where a null values are excluded */
+    /** Get a single value result where a null values are excluded
+     * @param timeout The maximum time to wait
+     * @param job Job with a simple value result
+     * @return The result stream
+     */
     public <R> Stream<R> getSingle(@Nonnull final Duration timeout, @Nonnull final Function<P, R> job)
             throws MultiRunException {
         return params.map(params -> CompletableFuture.supplyAsync(() -> job.apply(params)))
                 .collect(Collectors.toList()).stream() // For a parallel processing!
-                .map(createFunction(timeout))
+                .map(createGrabber(timeout))
                 .filter(Objects::nonNull);
     }
 
-    protected <R> Function<CompletableFuture<R>, R> createFunction(@Nonnull final Duration timeout) {
-        return (Grabber<R>) (CompletableFuture<R> t) -> t.get(timeout.toMillis(), MILLISECONDS);
+    /**
+     * @param timeout The maximum time to wait
+     */
+    protected <R> Function<CompletableFuture<R>, R> createGrabber(@Nonnull final Duration timeout)
+        return new Function<CompletableFuture<R>, R>() {
+
+            @Override
+            public R apply(@Nonnull final CompletableFuture<R> t) {
+                try {
+                    return convert(t);
+                } catch (InterruptedException | ExecutionException | TimeoutException e) {
+                    throw new MultiRunException(e);
+                }
+            }
+
+            protected R convert(@Nonnull final CompletableFuture<R> t)
+                    throws InterruptedException, ExecutionException, TimeoutException {
+                return t.get(timeout.toMillis(), MILLISECONDS);
+            }
+        };
     }
 
     // --- Static methods ---
@@ -84,21 +108,6 @@ public class MultiRun<P> {
     }
 
     // --- Class or Interfaces ---
-
-    /** Grab a result value from a CompletableFuture object */
-    protected interface Grabber<R> extends Function<CompletableFuture<R>, R> {
-
-        @Override
-        default R apply(@Nonnull final CompletableFuture<R> t) {
-            try {
-                return convert(t);
-            } catch (InterruptedException | ExecutionException | TimeoutException ex) {
-                throw new MultiRunException(ex);
-            }
-        }
-
-        R convert(CompletableFuture<R> t) throws InterruptedException, ExecutionException, TimeoutException;
-    }
 
     /** Internal exception */
     public static class MultiRunException extends IllegalStateException {
--- project-m2/ujo-tools/src/test/java/org/ujorm/tools/thread/MultiRunTest.java
+++ project-m2/ujo-tools/src/test/java/org/ujorm/tools/thread/MultiRunTest.java
@@ -20,16 +20,23 @@ package org.ujorm.tools.thread;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeoutException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.junit.Test;
+import org.ujorm.tools.thread.MultiRun.MultiRunException;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  *
  * @author Pavel Ponec
  */
 public class MultiRunTest {
+
+    private final Logger logger = Logger.getLogger(getClass().getName());
 
     /**
      * Test of getStream method, of class MultiRun.
@@ -61,6 +68,41 @@ public class MultiRunTest {
         List<Long> sortedList = stream.sorted().collect(Collectors.toList());
         assertEquals(6, sortedList.size());
         assertEquals(10L, sortedList.get(0).longValue());
+    }
+
+        /**
+     * Test of getSingle method, of class MultiRun.
+     */
+    @Test
+    public void testGetTimeout() {
+        System.out.println("getSingle");
+        Duration duration = Duration.ofMillis(100);
+        Exception result;
+        List<Long> sortedList = null;
+
+        try {
+           MultiRun<Integer> instance = MultiRun.params(100, 200, 400);
+           Stream<Long> stream = instance.getSingle(duration, (Integer p) -> sleep(p));
+           sortedList = stream.sorted().collect(Collectors.toList());
+           result = null;
+        } catch (Exception e) {
+            result = e;
+        }
+
+        assertTrue(result != null);
+        assertTrue(result.getClass().equals(MultiRunException.class));
+        assertTrue(result.getCause().getClass().equals(TimeoutException.class));
+        assertTrue(sortedList == null);
+    }
+
+    private Long sleep(Integer millis) {
+        try {
+            Thread.sleep(millis);
+            return (long) millis;
+        } catch (InterruptedException e) {
+            logger.log(Level.SEVERE, "An interrupting of the test", e);
+            return -1L;
+        }
     }
 
 
