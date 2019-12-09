98abfaaa581f9830c7d8be0d21dbc0cbb0cf30c3
Pavel Ponec <pponec@gmail.com>
2019-12-09 16:29:33 +0100

    TOOLS: MultiJob test cleaning

--- project-m2/ujo-tools/src/test/java/org/ujorm/tools/thread/MultiJobTest.java
+++ project-m2/ujo-tools/src/test/java/org/ujorm/tools/thread/MultiJobTest.java
@@ -38,8 +38,8 @@ public class MultiJobTest {
 
     private final Logger logger = Logger.getLogger(getClass().getName());
 
-    /** There is 1000 milliseconds in one second.  */
-    private final int MILLIS_IN_SEC = 1_000;
+    /** A time of the one task */
+    private final Duration TASK_TIME = Duration.ofSeconds(1);
 
     /**
      * Test of getStream method, of class MultiRun.
@@ -82,7 +82,7 @@ public class MultiJobTest {
         try {
             MultiJob.forEach(100, 200, 500)
                     .setTimeout(timeout)
-                    .run(p -> sleep(p))
+                    .run(p -> sleep(Duration.ofMillis(p)))
                     .collect(Collectors.toList());
         } catch (MultiJobException e) {
             result = e;
@@ -101,23 +101,23 @@ public class MultiJobTest {
         System.out.println("timeOfParalellWork");
 
         int jobCount = 100;
-        Integer[] params = new Integer[jobCount];
-        Arrays.fill(params, 1);
+        Duration[] params = new Duration[jobCount];
+        Arrays.fill(params, TASK_TIME);
 
         LocalDateTime start = LocalDateTime.now();
         List<Integer> list = MultiJob.forEach(params)
                 .setNewFixedThreadPool(jobCount)
-                .run(p -> sleep(p * MILLIS_IN_SEC)) // 1 sec
+                .run(duration -> sleep(duration)) // 1 sec
                    .collect(Collectors.toList());
         LocalDateTime stop = LocalDateTime.now();
 
         Duration duration = Duration.between(start, stop);
         assertTrue(String.format("Real working time was %s millis",
                 duration.toMillis()),
-                duration.toMillis() <= 1.15 * MILLIS_IN_SEC);
+                duration.toMillis() <= 1.15 * TASK_TIME.toMillis());
         assertTrue(String.format("Real working time was %s millis",
                 duration.toMillis()),
-                duration.toMillis() > 1.00 * MILLIS_IN_SEC);
+                duration.toMillis() > 1.00 * TASK_TIME.toMillis());
         assertEquals(jobCount, list.size());
         logger.log(Level.INFO, "Real working time was {0} seconds.", duration.getSeconds());
     }
@@ -131,20 +131,20 @@ public class MultiJobTest {
         System.out.println("timeOfSequentialWork");
 
         int jobCount = 10;
-        Integer[] params = new Integer[jobCount];
-        Arrays.fill(params, 1);
+        Duration[] params = new Duration[jobCount];
+        Arrays.fill(params, TASK_TIME);
 
         LocalDateTime start = LocalDateTime.now();
         List<Integer> list = MultiJob.forEach(Stream.of(params), false)
                 .setNewFixedThreadPool(jobCount)
-                .run(p -> sleep(p * MILLIS_IN_SEC)) // 1 sec
+                .run(duration -> sleep(duration)) // 1 sec
                    .collect(Collectors.toList());
         LocalDateTime stop = LocalDateTime.now();
 
         Duration duration = Duration.between(start, stop);
         assertTrue(String.format("Real working time was %s millis",
                 duration.toMillis()),
-                duration.toMillis() > jobCount * MILLIS_IN_SEC);
+                duration.toMillis() > jobCount * TASK_TIME.toMillis());
         assertEquals(jobCount, list.size());
         logger.log(Level.INFO, "Real working time was {0} seconds.", duration.getSeconds());
     }
@@ -157,33 +157,33 @@ public class MultiJobTest {
         System.out.println("timeOfParallelStream");
 
         int jobCount = 100;
-        Integer[] params = new Integer[jobCount];
-        Arrays.fill(params, 1);
+        Duration[] params = new Duration[jobCount];
+        Arrays.fill(params, TASK_TIME);
 
         LocalDateTime start = LocalDateTime.now();
         List<Integer> list = Arrays.stream(params)
                 .parallel()
-                .map(p -> sleep(p * MILLIS_IN_SEC)) // 1 sec
+                .map(dupration -> sleep(dupration)) // 1 sec
                 .collect(Collectors.toList());
         LocalDateTime stop = LocalDateTime.now();
 
         Duration duration = Duration.between(start, stop);
         assertTrue(String.format("Real working time was %s millis",
                 duration.toMillis()),
-                duration.toMillis() <= 15.5 * MILLIS_IN_SEC);
+                duration.toMillis() <= 15.5 * TASK_TIME.toMillis());
         assertEquals(jobCount, list.size());
         logger.log(Level.INFO, "Real working time was {0} seconds.", duration.getSeconds());
     }
 
-    private int sleep(int millis) {
+    private Integer sleep(Duration duration) {
         try {
-            Thread.sleep(millis);
+            Thread.sleep(duration.toMillis());
             logger.log(Level.INFO, "Sleeping {0} millis on {1}",
-                     new Object[]{ millis, LocalDateTime.now()} );
-            return millis;
+                     new Object[]{ duration.toMillis(), LocalDateTime.now()} );
+            return (int) duration.toMillis();
         } catch (InterruptedException e) {
             logger.log(Level.SEVERE, "An interrupting of the test", e);
-            return -1;
+            return null;
         }
     }
 }
--- project-m2/ujo-tools/src/test/java/org/ujorm/tools/thread/ParallelJobTest.java
+++ project-m2/ujo-tools/src/test/java/org/ujorm/tools/thread/ParallelJobTest.java
@@ -39,8 +39,8 @@ public class ParallelJobTest {
 
     private final Logger logger = Logger.getLogger(getClass().getName());
 
-    /** There is 1000 milliseconds in one second.  */
-    private final int MILLIS_IN_SEC = 1_000;
+    /** A time of the one task */
+    private final Duration TASK_TIME = Duration.ofSeconds(1);
 
     /**
      * Test of getStream method, of class MultiRun.
@@ -83,7 +83,7 @@ public class ParallelJobTest {
         try {
             ParallelJob.forEach(100, 200, 500)
                     .setTimeout(timeout)
-                    .run(p -> sleep(p))
+                    .run(p -> sleep(Duration.ofMillis((p))))
                     .collect(Collectors.toList());
         } catch (ParallelJobException e) {
             result = e;
@@ -104,23 +104,23 @@ public class ParallelJobTest {
         System.out.println("timeOfParalellWork");
 
         int jobCount = 100;
-        Integer[] params = new Integer[jobCount];
-        Arrays.fill(params, 1);
+        Duration[] params = new Duration[jobCount];
+        Arrays.fill(params, TASK_TIME);
 
         LocalDateTime start = LocalDateTime.now();
-        List<Long> list = ParallelJob.forEach(params)
+        List<Integer> list = ParallelJob.forEach(params)
                 .setExecutor(new ForkJoinPool(jobCount))
-                .run(p -> sleep(p * MILLIS_IN_SEC)) // 1 sec
+                .run(duration -> sleep(duration)) // 1 sec
                    .collect(Collectors.toList());
         LocalDateTime stop = LocalDateTime.now();
 
         Duration duration = Duration.between(start, stop);
         assertTrue(String.format("Real working time was %s millis",
                 duration.toMillis()),
-                duration.toMillis() <= 1.15 * MILLIS_IN_SEC);
+                duration.toMillis() <= 1.15 * TASK_TIME.toMillis());
         assertTrue(String.format("Real working time was %s millis",
                 duration.toMillis()),
-                duration.toMillis() > 1.00 * MILLIS_IN_SEC);
+                duration.toMillis() > 1.00 * TASK_TIME.toMillis());
         assertEquals(jobCount, list.size());
         logger.log(Level.INFO, "Real working time was {0} seconds.", duration.getSeconds());
     }
@@ -133,20 +133,20 @@ public class ParallelJobTest {
         System.out.println("timeOfSequentialWork");
 
         int jobCount = 10;
-        Integer[] params = new Integer[jobCount];
-        Arrays.fill(params, 1);
+        Duration[] params = new Duration[jobCount];
+        Arrays.fill(params, TASK_TIME);
 
         LocalDateTime start = LocalDateTime.now();
-        List<Long> list = ParallelJob.forEach(Stream.of(params), false)
+        List<Integer> list = ParallelJob.forEach(Stream.of(params), false)
                 .setNewFixedThreadPool(jobCount)
-                .run(p -> sleep(p * MILLIS_IN_SEC)) // 1 sec
+                .run(p -> sleep(TASK_TIME)) // 1 sec
                    .collect(Collectors.toList());
         LocalDateTime stop = LocalDateTime.now();
 
         Duration duration = Duration.between(start, stop);
         assertTrue(String.format("Real working time was %s millis",
                 duration.toMillis()),
-                duration.toMillis() > jobCount * MILLIS_IN_SEC);
+                duration.toMillis() > jobCount * TASK_TIME.toMillis());
         assertEquals(jobCount, list.size());
         logger.log(Level.INFO, "Real working time was {0} seconds.", duration.getSeconds());
     }
@@ -159,33 +159,33 @@ public class ParallelJobTest {
         System.out.println("timeOfParallelStream");
 
         int jobCount = 100;
-        Integer[] params = new Integer[jobCount];
-        Arrays.fill(params, 1);
+        Duration[] params = new Duration[jobCount];
+        Arrays.fill(params, TASK_TIME);
 
         LocalDateTime start = LocalDateTime.now();
-        List<Long> list = Arrays.stream(params)
+        List<Integer> list = Arrays.stream(params)
                 .parallel()
-                .map(p -> sleep(p * MILLIS_IN_SEC)) // 1 sec
+                .map(duration -> sleep(duration)) // 1 sec
                 .collect(Collectors.toList());
         LocalDateTime stop = LocalDateTime.now();
 
         Duration duration = Duration.between(start, stop);
         assertTrue(String.format("Real working time was %s millis",
                 duration.toMillis()),
-                duration.toMillis() <= 15.5 * MILLIS_IN_SEC);
+                duration.toMillis() <= 15.5 * TASK_TIME.toMillis());
         assertEquals(jobCount, list.size());
         logger.log(Level.INFO, "Real working time was {0} seconds.", duration.getSeconds());
     }
 
-    private long sleep(int millis) {
+    private Integer sleep(Duration duration) {
         try {
-            Thread.sleep(millis);
+            Thread.sleep(duration.toMillis());
             logger.log(Level.INFO, "Sleeping {0} millis on {1}",
-                     new Object[]{ millis, LocalDateTime.now()} );
-            return millis;
+                     new Object[]{ duration.toMillis(), LocalDateTime.now()} );
+            return (int) duration.toMillis();
         } catch (InterruptedException e) {
             logger.log(Level.SEVERE, "An interrupting of the test", e);
-            return -1L;
+            return null;
         }
     }
 }
