9b7011e959f5f10da6a9c1c0586b13a1c7e39217
Pavel Ponec <pponec@gmail.com>
2019-12-17 19:28:03 +0100

    TOOLS: MultiJob cleaning

--- project-m2/ujo-tools/src/main/java/org/ujorm/tools/thread/MultiJob.java
+++ project-m2/ujo-tools/src/main/java/org/ujorm/tools/thread/MultiJob.java
@@ -183,8 +183,8 @@ public class MultiJob<P> {
 
     /**
      * A factory method for a multithreading instance
-     * @param maxThreadCount A count of threads where a zero value uses a default ThreadPool and a negative value invoke run on the current thread.
      * @param params All aguments
+     * @param maxThreadCount A count of threads where a zero value uses a default ThreadPool and a negative value invoke run on the current thread.
      * @return An instance of MultiJob
      */
     public static <P> MultiJob<P> forEach(@Nonnull final P[] params, final int maxThreadCount) {
