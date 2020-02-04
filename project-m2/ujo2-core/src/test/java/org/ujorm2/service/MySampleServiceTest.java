fa52d91b522b2291645f0c3f911488d4d0f8b7bb
Pavel Ponec <pponec@gmail.com>
2020-02-04 16:30:46 +0100

    Impl

---
+++ project-m2/ujo2-core/src/test/java/org/ujorm2/service/MySampleServiceTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2020 pavel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ujorm2.service;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author pavel
+ */
+public class MySampleServiceTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of doOrderAccess method, of class MySampleService.
+     */
+    @Test
+    public void testDoOrderAccess() {
+        System.out.println("doOrderAccess");
+        MySampleService instance = instance();
+        instance.doOrderAccess();
+    }
+
+    /**
+     * Test of doItemAccess method, of class MySampleService.
+     */
+    @Test
+    public void testDoItemAccess() {
+        System.out.println("doItemAccess");
+        MySampleService instance = instance();
+        instance.doItemAccess();
+    }
+
+    /**
+     * Test of doItemCondition method, of class MySampleService.
+     */
+    @Test
+    public void testDoItemCondition() {
+        System.out.println("doItemCondition");
+        MySampleService instance = instance();
+        instance.doItemCondition();
+    }
+
+    private MySampleService instance() {
+        return new MySampleService();
+    }
+
+}
