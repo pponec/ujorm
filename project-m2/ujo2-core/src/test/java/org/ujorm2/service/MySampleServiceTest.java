218b7c680c4d7e1150cd758dd412d9acd1c65b87
Pavel Ponec <pponec@gmail.com>
2020-02-04 18:23:20 +0100

    Impl + Test

--- project-m2/ujo2-core/src/test/java/org/ujorm2/service/MySampleServiceTest.java
+++ project-m2/ujo2-core/src/test/java/org/ujorm2/service/MySampleServiceTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 pavel.
+ * Copyright 2020 Pavel Ponec
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
