c0d45ed087e22351c550d9eeaca75e153616032f
Pavel Ponec <pponec@gmail.com>
2020-02-27 18:42:03 +0100

    TOOLS: A common multithreading support

--- project-m2/ujo-tools/src/test/java/org/ujorm/tools/thread/SampleTest.java
+++ project-m2/ujo-tools/src/test/java/org/ujorm/tools/thread/SampleTest.java
@@ -3,7 +3,6 @@ package org.ujorm.tools.thread;
 import io.reactivex.Flowable;
 import io.reactivex.schedulers.Schedulers;
 import java.time.Duration;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -16,7 +15,6 @@ import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /** Tested on JRE 1.8.0_231 */
@@ -33,20 +31,44 @@ public class SampleTest {
         System.out.println(msg);
     }
 
+    /** 1_000 jobs take 1.2 seconds by 1_000 threads,
+     * 10_000 jobs take 9.2 seconds by 10_000 threads,
+     * 32_767 threads throws java.lang.OutOfMemoryError.
+     */
+    @Test(timeout = 1_500)
+    public void completableFutureTest() {
+
+        final int parallelism = 1_000; // Short.MAX_VALUE; // 10_000;
+        final Duration jobDuration = Duration.ofSeconds(1);
+        final ExecutorService threadPool = Executors.newFixedThreadPool(parallelism);
+        final IntStream params = IntStream.rangeClosed(1, parallelism);
+        final int sum = params
+                .boxed()
+                .map(t -> CompletableFuture.supplyAsync(() -> sleep(t, jobDuration), threadPool))
+                .collect(Collectors.toList()).stream() // For parallel processing!
+                .map(CompletableFuture::join)
+                .mapToInt(i -> i)
+                .sum();
+        threadPool.shutdown();
+        System.out.println(sum);
+    }
+
     /** 1_000 jobs take 32 seconds on ForkJoinPool with 1_000 parallelism.
      * 10_000 jobs take 313 seconds on ForkJoinPool with 10_000 parallelism,
      * 32_767 jobs take 313 seconds on ForkJoinPool with 32_767 parallelism,
      */
-    @Test
+    @Test(timeout = 3_300)
     public void parallelStreamTest() throws InterruptedException, ExecutionException {
         System.out.println("parallelStreamTest");
 
         final int parallelism = 100; // 32_767;
         final Duration jobDuration = Duration.ofSeconds(1);
         final ForkJoinPool threadPool = new ForkJoinPool(parallelism);
-        final int sum = threadPool.submit(() -> Collections.nCopies(parallelism, 1)
-                        .parallelStream()
-                        .map(t -> job(t, jobDuration))
+        final IntStream params = IntStream.rangeClosed(1, parallelism);
+        final int sum = threadPool.submit(() -> params
+                        .boxed()
+                        .parallel()
+                        .map(t -> sleep(t, jobDuration))
                         .mapToInt(i -> i)
                         .sum()
         ).get();
@@ -55,31 +77,27 @@ public class SampleTest {
         System.out.println(sum);
     }
 
-    /** 1_000 jobs take 1.2 seconds by 1_000 threads,
-     * 10_000 jobs take 9.2 seconds by 10_000 threads,
-     * 32_767 threads throws java.lang.OutOfMemoryError.
-     */
-    @Test
-    public void completableFutureTest() {
+    /** 1_000 jobs take 127 seconds on DEFAULT ForkJoinPool. */
+    @Test(timeout = 8_000)
+    public void parallelStreamDefaultPoolTest() {
 
-        final int parallelism = 1_000; // Short.MAX_VALUE; // 10_000;
+        final int parallelism = 50; //!!!
         final Duration jobDuration = Duration.ofSeconds(1);
-        final ExecutorService threadPool = Executors.newFixedThreadPool(parallelism);
-        final int sum = Collections.nCopies(parallelism, 1)
-                .stream()
-                .map(t -> CompletableFuture.supplyAsync(() -> job(t, jobDuration), threadPool))
-                .collect(Collectors.toList()).stream() // For parallel processing!
-                .map(CompletableFuture::join)
+        final int sum = IntStream.rangeClosed(1, parallelism)
+                .boxed()
+                .parallel()
+                .map(t -> sleep(t, jobDuration))
                 .mapToInt(i -> i)
                 .sum();
-        threadPool.shutdown();
+
         System.out.println(sum);
     }
+
     /** 1_000 jobs take 1.2 seconds by 1_000 threads,
      * 10_000 jobs take 9.2 seconds by 10_000 threads,
      * 32_767 threads throws java.lang.OutOfMemoryError.
      */
-    @Test
+    @Test(timeout = 1_800)
     public void runnableTest() {
 
         final int parallelism = 10_000; // Short.MAX_VALUE; // 10_000;
@@ -87,42 +105,15 @@ public class SampleTest {
         final AtomicInteger sum = new AtomicInteger();
         final ThreadGroup tg = new ThreadGroup ("myThreadGroup");
 
-        Collections.nCopies(parallelism, 1)
-                .stream()
-                .forEach(i -> new Thread(tg, () -> sum.addAndGet(job(i, jobDuration)))
+        IntStream.rangeClosed(1, parallelism)
+                .forEach(i -> new Thread(tg, () -> sum.addAndGet(sleep(i, jobDuration)))
                 .start());
 
         while (tg.activeCount() > 0) {
-            job(1, Duration.ofMillis(100));
+            sleep(1, Duration.ofMillis(100));
         }
 
         System.out.println(sum);
-    }
-
-    /** 1_000 jobs take 127 seconds on DEFAULT ForkJoinPool. */
-    @Ignore
-    @Test
-    public void defaultThreadPoolTest() {
-
-        final int parallelism = 1_000;
-        final Duration jobDuration = Duration.ofSeconds(1);
-        final int sum = Collections.nCopies(parallelism, 1)
-                .parallelStream()
-                .map(t -> job(t, jobDuration))
-                .mapToInt(i -> i)
-                .sum();
-
-        System.out.println(sum);
-    }
-
-    private int job(int value, Duration duration) {
-        try {
-            logger.log(Level.FINEST, "Sleeping {0} sec", duration.getSeconds());
-            Thread.sleep(duration.toMillis());
-            return value;
-        } catch (InterruptedException e) {
-            throw new IllegalStateException("An interruption of the test", e);
-        }
     }
 
     /** ReactiveX / RxJava */
@@ -145,5 +136,17 @@ public class SampleTest {
 
         builder.stream()
                 .forEach(v -> System.out.println(">>> result: " +  v));
+    }
+
+    // --- TOOLS ---
+
+    private int sleep(int value, Duration duration) {
+        try {
+            logger.log(Level.FINEST, "Sleeping {0} sec", duration.getSeconds());
+            Thread.sleep(duration.toMillis());
+             return value;
+        } catch (InterruptedException e) {
+            throw new IllegalStateException("An interruption of the test", e);
+        }
     }
 }
