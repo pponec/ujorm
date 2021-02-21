1063016eabbdcb6be11953dc057f4a623e168c82
Pavel Ponec <pponec@gmail.com>
2021-02-21 22:23:34 +0100

    WEB: ES6 Vanilla Javascript Writer

--- project-m2/ujo-web/src/main/java/org/ujorm/tools/web/ajax/JavaScriptWriter.java
+++ project-m2/ujo-web/src/main/java/org/ujorm/tools/web/ajax/JavaScriptWriter.java
@@ -198,7 +198,7 @@ public class JavaScriptWriter implements Injector {
                 , "  .then(response=>response.json())"
                 , "  .then(data=>{"
                 , "    for (var key of Object.keys(data)) {"
-                , "      document.querySelector(key).innerHTML=data[key];"
+                , "      document.querySelectorAll(key).forEach(i=>{i.innerHTML=data[key];})"
                 , "    }"
                 , "    if(submitReq){submitReq=false;process(e);}" // Next submit the form
                 , "    else{ajaxRun=false;}"
