03bda6abcf5caf33047420c44b41a6673c6b7b01
Pavel Ponec <pponec@gmail.com>
2021-04-02 10:41:46 +0200

    SAMPLES: jUnit test is fixed

--- samples/x2j-converter/src/test/java/net/ponec/demo/DemoElementApplicationTests.java
+++ samples/x2j-converter/src/test/java/net/ponec/demo/DemoElementApplicationTests.java
@@ -1,9 +1,10 @@
 package net.ponec.demo;
 
+import net.ponec.x2j.Application;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Application.class)
 class DemoElementApplicationTests {
 
     @Test
