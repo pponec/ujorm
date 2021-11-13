6dd648020629f0ddcb2b020789b28ed5c885a738
Pavel Ponec <pponec@gmail.com>
2021-11-13 17:22:19 +0100

    COMMON: Maven refactoring

--- project-m2/ujo-tools/src/test/java/org/ujorm/tools/common/JoinableTest.java
+++ project-m2/ujo-tools/src/test/java/org/ujorm/tools/common/JoinableTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 pavel.
+ * Copyright 2021 Pavel Ponec.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,8 +16,9 @@
 package org.ujorm.tools.common;
 
 import java.util.function.Function;
-import static org.junit.Assert.*;
 import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  *
