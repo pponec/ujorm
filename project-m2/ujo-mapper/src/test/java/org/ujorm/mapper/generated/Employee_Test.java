da763aca039fcee17b05a921fea86ed531f69f91
pavel.ponec <pavel.ponec@gmail.com>
2026-02-17 12:00:25 +0100

    jUnit test: ElementUjo

--- project-m2/ujo-mapper/src/test/java/org/ujorm/mapper/generated/Employee_Test.java
+++ project-m2/ujo-mapper/src/test/java/org/ujorm/mapper/generated/Employee_Test.java
@@ -2,8 +2,10 @@ package org.ujorm.mapper.generated;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.ujorm.core.Key;
 import org.ujorm.mapper.generated.demo.City;
 import org.ujorm.mapper.generated.demo.Employee;
+import java.time.LocalDate;
 
 class Employee_Test {
 
@@ -39,5 +41,58 @@ class Employee_Test {
         Assertions.assertFalse(keyActive.getDefaultValue());
         Assertions.assertFalse(keyActive.isDefault(employee));
 
+    }
+
+    /** Create new instance */
+    @Test
+    void testNewInstance_basic() {
+        var e = new ElementUjo();
+        e.setValue(e.keyId, 10L);
+        e.setValue(e.keyName, "Test");
+        e.setValue(e.keySuperior, null);
+        e.setValue(e.keyCity, City.of(1L, "California"));
+        e.setValue(e.keyContractDay, LocalDate.of(2026, 02, 17));
+        e.setValue(e.keyActive, true);
+        var employee = e.newInstance();
+
+        Assertions.assertEquals(10, employee.getId());
+        Assertions.assertEquals("Test", employee.getName());
+        Assertions.assertEquals(null, employee.getSuperior());
+        Assertions.assertEquals("California", employee.getCity().name());
+        Assertions.assertEquals(2026, employee.getContractDay().getYear());
+        Assertions.assertEquals(true, employee.isActive());
+    }
+
+    /** Create new instance */
+    @Test
+    void testNewInstance_null() {
+        var e = new ElementUjo();
+        var employee = e.newInstance();
+
+        Assertions.assertNull(employee.getId());
+        Assertions.assertNull(employee.getName());
+        Assertions.assertNull(employee.getCity());
+        Assertions.assertNull(employee.getSuperior());
+        Assertions.assertNull(employee.getContractDay());
+        Assertions.assertEquals(false, employee.isActive());
+    }
+
+    public class ElementUjo {
+        final Employee_ meta = new Employee_();
+        final Object[] array = new Object[meta.count()];
+        final Key<Employee, Long> keyId = meta.getKey("id", Long.class);
+        final Key<Employee, String> keyName = meta.getKey("name", String.class);
+        final Key<Employee, Employee> keySuperior = meta.getKey("superior", Employee.class);
+        final Key<Employee, City> keyCity = meta.getKey("city", City.class);
+        final Key<Employee, LocalDate> keyContractDay = meta.getKey("contractDay", LocalDate.class);
+        final Key<Employee, Boolean> keyActive = meta.getKey("active", boolean.class);
+
+        public <V> void setValue(Key<Employee,V> key, V value) {
+            array[key.getIndex()] = value;
+        }
+
+        public Employee newInstance() {
+            return meta.newDomain(array);
+        }
     }
 }
