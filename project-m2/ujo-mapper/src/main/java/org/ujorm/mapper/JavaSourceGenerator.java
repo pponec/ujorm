93bd55281633b8b64c11a72c6f5b5a1d6ca94330
pavel.ponec <pavel.ponec@gmail.com>
2026-02-17 14:07:53 +0100

    JavaSourceGenerator.java

--- project-m2/ujo-mapper/src/main/java/org/ujorm/mapper/JavaSourceGenerator.java
+++ project-m2/ujo-mapper/src/main/java/org/ujorm/mapper/JavaSourceGenerator.java
@@ -1,11 +1,9 @@
 package org.ujorm.mapper;
 
-import lombok.Locked;
 import org.ujorm.tools.msg.MessageService;
 
 import java.io.StringWriter;
 import java.util.HashMap;
-import java.util.Locale;
 
 public class JavaSourceGenerator {
 
@@ -20,76 +18,86 @@ public class JavaSourceGenerator {
         var params = new HashMap<String, Object>();
         {
             params.put("package", PACKAGE_PREFIX + meta.beanClass().getPackageName());
-            params.put("class", meta.beanClass().getSimpleName());
+            params.put("generatedClass", meta.beanClass().getSimpleName() + "_");
             params.put("domainClass", meta.beanClass().getName());
             params.put("keys", "");
         }
-        var templateBeg = """
-            package org.ujorm.mapper.generated;
+        var templateBeg1 = """
+            package ${package};
             import org.jetbrains.annotations.NotNull;
             import org.jetbrains.annotations.Nullable;
             import org.ujorm.core.Key;
             import org.ujorm.core.impl.AbstractKey;
             import org.ujorm.core.impl.AbstractMetaModel;
             
-            public class Employee_ extends AbstractMetaModel<Employee> {
-                private static final Class<Employee> domainType = Employee.class;
-                public Employee_() {
-                    super( new Key_id(0)
-                         , new Key_name(1)
-                         , new Key_city(2)
-                         , new Key_superior(3)
-                         , new Key_contractDay(4)
-                         , new Key_active(5)
+            public class ${generatedClass} extends AbstractMetaModel<${domainClass}> {
+                private static final Class<${domainClass}> domainType = ${domainClass}.class;
+                public ${generatedClass}() {
+                """;
+//                    super( new Key_id(0)
+//                         , new Key_name(1)
+             var templateBeg2 = """
                          );
                 }
-            
                 @Override
-                public Employee newDomain(@NotNull final Object... values) {
-                    final var result = new Employee();
+                public ${domainClass} newDomain(@NotNull final Object... values) {
+                    final var result = new ${domainClass}();
                     for (int i = 0, max = Math.min(values.length, keyList.size()); i < max; i++) {
-                        final var key = (Key<Employee, Object>) keyList.get(i);
+                        final var key = (Key<${domainClass}, Object>) keyList.get(i);
                         key.setValue(result, values[i]);
                     }
                     return result;
                 }
             
                 @NotNull
-                public Class<Employee> getDomainType() {
+                public Class<${domainClass}> getDomainType() {
                     return domainType;
                 }
                 """;
 
         var templateMid = """           
                 /** Key id */
-                static final class Key_id extends AbstractKey<Employee, Long> {
+                static final class Key_id extends AbstractKey<${domainClass}, Long> {
                     public Key_id(int order) {
                         super(order, "id", Long.class, "id", true, true);
                     }
                     @Override
-                    public void setValue(@NotNull final Employee bean, @Nullable final Long value) {
+                    public void setValue(@NotNull final ${domainClass} bean, @Nullable final Long value) {
                         bean.setId(value != null ? value : defaultValue);
                     }
                     @Override
-                    public Long getValue(@NotNull final Employee bean) {
+                    public Long getValue(@NotNull final ${domainClass} bean) {
                         return bean.getId();
                     }
                     @Override
-                    public @NotNull Class<Employee> getDomainType() {
+                    public @NotNull Class<${domainClass}> getDomainType() {
                         return domainType;
                     }
                 }
                 """;
         var templateEnd = "}";
 
-        MessageService.formatMsg(templateBeg, params, writer);
-        formatMiddle(templateMid, meta, writer);
+        MessageService.formatMsg(templateBeg1, params, writer);
+        buildConstructor(meta, writer);
+        MessageService.formatMsg(templateBeg2, params, writer);
+        buildInnerKeys(templateMid, meta, writer);
         MessageService.formatMsg(templateEnd, params, writer);
-
         return writer.toString();
     }
 
-    private void formatMiddle(String template, DomainModel meta, StringWriter writer) {
+    private void buildConstructor(DomainModel meta, StringWriter writer) {
+        for(int i = 0, max = meta.properties().size(); i < max; ++i) {
+            var row = i == 0
+                    ? "super( new Key_%s(%s)"
+                    : "     , new Key_%s(%s)";
+            writer.append("        ")
+                    .append(row.formatted(meta.properties().get(i), i))
+                    .write("\n");
+
+        }
+    }
+
+    private void buildInnerKeys(String template, DomainModel meta, StringWriter writer) {
         final var params = new HashMap<String, Object>();
         for (var prop: meta.properties()) {
             {
@@ -99,7 +107,6 @@ public class JavaSourceGenerator {
                 params.put("domainClass", meta.beanClass().getName());
             }
             MessageService.formatMsg(template, params, writer);
-
         }
     }
 
