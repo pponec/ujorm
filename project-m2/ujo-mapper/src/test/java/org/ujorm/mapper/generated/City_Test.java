b5a1e41b0b4519b35716627c7f4fcdfe40848afc
pavel.ponec <pavel.ponec@gmail.com>
2026-02-17 12:39:21 +0100

    jUnit test: ElementUjo

---
+++ project-m2/ujo-mapper/src/test/java/org/ujorm/mapper/generated/City_Test.java
@@ -0,0 +1,61 @@
+package org.ujorm.mapper.generated;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.ujorm.core.Key;
+import org.ujorm.mapper.generated.demo.City;
+
+class City_Test {
+
+    private static double DELTA = 0.00_000_000_001;
+
+
+    /** Create new instance */
+    @Test
+    void testNewInstance_basic() {
+        var e = new CityUjo();
+        e.setValue(CityUjo.keyId, 10L);
+        e.setValue(CityUjo.keyName, "Ottawa");
+        e.setValue(CityUjo.keyCountryCode, "CA");
+        e.setValue(CityUjo.keyLatitude, 45.4215);
+        e.setValue(CityUjo.keyLongitude, -75.6972);
+        var city = e.newInstance();
+
+        Assertions.assertEquals(10, city.id());
+        Assertions.assertEquals("Ottawa", city.name());
+        Assertions.assertEquals("CA", city.countryCode());
+        Assertions.assertEquals(45.4215, city.longitude(), DELTA);
+        Assertions.assertEquals(-75.6972, city.latitude(), DELTA);
+    }
+
+    /** Create new instance */
+    @Test
+    void testNewInstance_null() {
+        var e = new CityUjo();
+        var city = e.newInstance();
+
+        Assertions.assertNull(city.id());
+        Assertions.assertNull(city.name());
+        Assertions.assertNull(city.countryCode());
+        Assertions.assertEquals(0.0, city.longitude(), DELTA);
+        Assertions.assertEquals(0.0, city.latitude(), DELTA);
+    }
+
+    public class CityUjo {
+        static final City_ meta = new City_();
+        static final Key<City, Long> keyId = meta.getKey("id", Long.class);
+        static final Key<City, String> keyName = meta.getKey("name", String.class);
+        static final Key<City, String> keyCountryCode = meta.getKey("countryCode", String.class);
+        static final Key<City, Double> keyLatitude = meta.getKey("latitude", Double.class);
+        static final Key<City, Double> keyLongitude = meta.getKey("longitude", Double.class);
+        final Object[] array = new Object[meta.count()];
+
+        public <V> void setValue(Key<City,V> key, V value) {
+            array[key.getIndex()] = value;
+        }
+
+        public City newInstance() {
+            return meta.newDomain(array);
+        }
+    }
+}
--- project-m2/ujo-mapper/src/test/java/org/ujorm/mapper/generated/Employee_Test.java
+++ project-m2/ujo-mapper/src/test/java/org/ujorm/mapper/generated/Employee_Test.java
@@ -15,50 +15,45 @@ class Employee_Test {
         Assertions.assertEquals(Employee.class, instance.getDomainType());
         Assertions.assertEquals(6, instance.count());
 
-        var keyId = instance.getKey("id", Long.class);
-        var keyName = instance.getKey("name", String.class);
-        var keyActive = instance.getKey("active", boolean.class);
-        var keyCity = instance.getKey("city", City.class);
         var employee = instance.newDomain();
-
-        keyId.setValue(employee, 10L);
-        keyName.setValue(employee, "Joe");
-        keyCity.setValue(employee, City.of(9L, "California"));
-        keyActive.setValue(employee, true);
+        EmplyeeUjo.keyId.setValue(employee, 10L);
+        EmplyeeUjo.keyName.setValue(employee, "Joe");
+        EmplyeeUjo.keyCity.setValue(employee, City.of(9L, "Ottawa"));
+        EmplyeeUjo.keyActive.setValue(employee, true);
 
         Assertions.assertEquals(10, employee.getId());
         Assertions.assertEquals("Joe", employee.getName());
         Assertions.assertEquals(true, employee.isActive());
         Assertions.assertEquals("California", employee.getCity().name());
 
-        Assertions.assertEquals(10, keyId.getValue(employee));
-        Assertions.assertEquals("Joe", keyName.getValue(employee));
-        Assertions.assertEquals(true, keyActive.getValue(employee));
-        Assertions.assertEquals("California", keyCity.getValue(employee).name());
+        Assertions.assertEquals(10, EmplyeeUjo.keyId.getValue(employee));
+        Assertions.assertEquals("Joe", EmplyeeUjo.keyName.getValue(employee));
+        Assertions.assertEquals(true, EmplyeeUjo.keyActive.getValue(employee));
+        Assertions.assertEquals("California", EmplyeeUjo.keyCity.getValue(employee).name());
 
-        Assertions.assertNull(keyCity.getDefaultValue());
-        Assertions.assertFalse(keyCity.isDefault(employee));
-        Assertions.assertFalse(keyActive.getDefaultValue());
-        Assertions.assertFalse(keyActive.isDefault(employee));
+        Assertions.assertNull(EmplyeeUjo.keyCity.getDefaultValue());
+        Assertions.assertFalse(EmplyeeUjo.keyCity.isDefault(employee));
+        Assertions.assertFalse(EmplyeeUjo.keyActive.getDefaultValue());
+        Assertions.assertFalse(EmplyeeUjo.keyActive.isDefault(employee));
 
     }
 
     /** Create new instance */
     @Test
     void testNewInstance_basic() {
-        var e = new ElementUjo();
-        e.setValue(e.keyId, 10L);
-        e.setValue(e.keyName, "Test");
-        e.setValue(e.keySuperior, null);
-        e.setValue(e.keyCity, City.of(1L, "California"));
-        e.setValue(e.keyContractDay, LocalDate.of(2026, 02, 17));
-        e.setValue(e.keyActive, true);
+        var e = new EmplyeeUjo();
+        e.setValue(EmplyeeUjo.keyId, 10L);
+        e.setValue(EmplyeeUjo.keyName, "Test");
+        e.setValue(EmplyeeUjo.keySuperior, null);
+        e.setValue(EmplyeeUjo.keyCity, City.of(1L, "Ottawa"));
+        e.setValue(EmplyeeUjo.keyContractDay, LocalDate.of(2026, 02, 17));
+        e.setValue(EmplyeeUjo.keyActive, true);
         var employee = e.newInstance();
 
         Assertions.assertEquals(10, employee.getId());
         Assertions.assertEquals("Test", employee.getName());
         Assertions.assertEquals(null, employee.getSuperior());
-        Assertions.assertEquals("California", employee.getCity().name());
+        Assertions.assertEquals("Ottawa", employee.getCity().name());
         Assertions.assertEquals(2026, employee.getContractDay().getYear());
         Assertions.assertEquals(true, employee.isActive());
     }
@@ -66,7 +61,7 @@ class Employee_Test {
     /** Create new instance */
     @Test
     void testNewInstance_null() {
-        var e = new ElementUjo();
+        var e = new EmplyeeUjo();
         var employee = e.newInstance();
 
         Assertions.assertNull(employee.getId());
@@ -77,15 +72,15 @@ class Employee_Test {
         Assertions.assertEquals(false, employee.isActive());
     }
 
-    public class ElementUjo {
-        final Employee_ meta = new Employee_();
+    public class EmplyeeUjo {
+        static final Employee_ meta = new Employee_();
+        static final Key<Employee, Long> keyId = meta.getKey("id", Long.class);
+        static final Key<Employee, String> keyName = meta.getKey("name", String.class);
+        static final Key<Employee, Employee> keySuperior = meta.getKey("superior", Employee.class);
+        static final Key<Employee, City> keyCity = meta.getKey("city", City.class);
+        static final Key<Employee, LocalDate> keyContractDay = meta.getKey("contractDay", LocalDate.class);
+        static final Key<Employee, Boolean> keyActive = meta.getKey("active", boolean.class);
         final Object[] array = new Object[meta.count()];
-        final Key<Employee, Long> keyId = meta.getKey("id", Long.class);
-        final Key<Employee, String> keyName = meta.getKey("name", String.class);
-        final Key<Employee, Employee> keySuperior = meta.getKey("superior", Employee.class);
-        final Key<Employee, City> keyCity = meta.getKey("city", City.class);
-        final Key<Employee, LocalDate> keyContractDay = meta.getKey("contractDay", LocalDate.class);
-        final Key<Employee, Boolean> keyActive = meta.getKey("active", boolean.class);
 
         public <V> void setValue(Key<Employee,V> key, V value) {
             array[key.getIndex()] = value;
