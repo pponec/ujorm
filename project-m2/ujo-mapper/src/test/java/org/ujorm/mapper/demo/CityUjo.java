e416add2ca524a4bfee519b8498f6236474ec3e8
pavel.ponec <pavel.ponec@gmail.com>
2026-02-19 08:59:46 +0100

    Code cleaning

--- project-m2/ujo-mapper/src/test/java/org/ujorm/mapper/demo/CityUjo.java
+++ project-m2/ujo-mapper/src/test/java/org/ujorm/mapper/demo/CityUjo.java
@@ -21,10 +21,7 @@ import org.ujorm.mapper.demo.generated.City_;
  */
 public class CityUjo implements Ujo<City>  {
     /** For implementation see the {@link City_} class. */
-    public static final DomainHandler<City> meta = false
-            ? DomainHandlerProvider.getHandler(City.class)
-            : new City_();
-
+    public static final DomainHandler<City> meta = DomainHandlerProvider.getHandler(City.class);
     public static final Key<City, Long> keyId = meta.getKey("id", Long.class);
     public static final Key<City, String> keyName = meta.getKey("name", String.class);
     public static final Key<City, String> keyCountryCode = meta.getKey("countryCode", String.class);
--- project-m2/ujo-mapper/src/test/java/org/ujorm/mapper/demo/EmployeeUjo.java
+++ project-m2/ujo-mapper/src/test/java/org/ujorm/mapper/demo/EmployeeUjo.java
@@ -23,10 +23,7 @@ import java.time.LocalDate;
  */
 public class EmployeeUjo implements Ujo<Employee> {
     /** For implementation see the {@link Employee_} class. */
-    public static final DomainHandler<Employee> meta = false
-            ? DomainHandlerProvider.getHandler(Employee.class)
-            : new Employee_();
-
+    public static final DomainHandler<Employee> meta = DomainHandlerProvider.getHandler(Employee.class);
     public static final Key<Employee, Long> keyId = meta.getKey("id", Long.class);
     public static final Key<Employee, String> keyName = meta.getKey("name", String.class);
     public static final Key<Employee, Employee> keySuperior = meta.getKey("superior", Employee.class);
