27a53df6fe5f0d773ea1f04a819b741aee82078e
pavel.ponec <pavel.ponec@gmail.com>
2026-02-23 15:39:10 +0100

    UPDATE

---
+++ project-m2/ujo-jdbc-mapper/src/test/java/org/ujorm/mapper/core/EntityManagerTest.java
@@ -0,0 +1,61 @@
+package org.ujorm.mapper.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.ujorm.mapper.demo.City;
+import org.ujorm.mapper.demo.Employee;
+
+import java.time.LocalDate;
+
+class EntityManagerTest extends AbstractDaoTest {
+
+    @Test
+    void crud() {
+        var cityDao = new EntityManager<City, Long>(City.class, dbConnection);
+        var cityInp = new City(null, "California", "US", 36.7783, -119.4179);
+        var cityOut = cityDao.insert(cityInp);
+        Assertions.assertNotNull(cityOut.id());
+        Assertions.assertEquals(1L, cityOut.id());
+        Assertions.assertNotSame(cityInp, cityOut);
+
+        // City including ID
+        cityInp = new City(101L, "Ottawa", "CA", 45.4215, 75.6972);
+        cityOut = cityDao.insert(cityInp);
+        Assertions.assertNotNull(cityOut.id());
+        Assertions.assertEquals(101L, cityOut.id());
+        Assertions.assertSame(cityInp, cityOut);
+
+        // Employee A
+        var emplDao = new EntityManager<Employee, Long>(Employee.class, dbConnection);
+        var employeeInp = createEmployee("EmplA", cityInp);
+        var employeeOut = emplDao.insert(employeeInp);
+        Assertions.assertNotNull(employeeOut.getId());
+        Assertions.assertEquals(1L, employeeOut.getId());
+        Assertions.assertSame(employeeInp, employeeOut);
+
+        // Employee B
+        employeeInp = createEmployee(101L, "EmplB", cityOut);
+        employeeOut = emplDao.insert(employeeInp);
+        Assertions.assertNotNull(employeeOut.getId());
+        Assertions.assertEquals(101L, employeeOut.getId());
+        Assertions.assertSame(employeeInp, employeeOut);
+
+        // Update-1
+        employeeOut.setName("EmplC");
+        long count = emplDao.update(employeeOut, "name");
+        Assertions.assertEquals(1, count);
+
+        // Update-2
+        employeeOut.setName("EmplC");
+        count = emplDao.update(employeeOut, "city", "name");
+        Assertions.assertEquals(1, count);
+    }
+
+    public Employee createEmployee(String name, City city) {
+        return createEmployee(null, name, city);
+    }
+
+    public Employee createEmployee(Long id, String name, City city) {
+        return new Employee(id, name, null , city, LocalDate.of(2020,1,1), true);
+    }
+}
