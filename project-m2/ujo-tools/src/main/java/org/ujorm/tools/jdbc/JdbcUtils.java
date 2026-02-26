f182f1bab2b70a427d43b5c2ed368182ddd5f63e
pavel.ponec <pavel.ponec@gmail.com>
2026-02-26 14:42:43 +0100

    JdbcUtils.java

---
+++ project-m2/ujo-tools/src/main/java/org/ujorm/tools/jdbc/JdbcUtils.java
@@ -0,0 +1,68 @@
+package org.ujorm.tools.jdbc;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class JdbcUtils {
+
+    /** Creates a Stream from the ResultSet. The Stream ensures the ResultSet is closed when finished. */
+    @NotNull
+    public static Stream<ResultSet> stream(final ResultSet rs) {
+        final var iterator = new Iterator<ResultSet>() {
+            @Override
+            public boolean hasNext() {
+                try {
+                    return rs.next();
+                } catch (SQLException e) {
+                    throw SQLExceptionBuilder.build(e);
+                }
+            }
+            @Override
+            public ResultSet next() {
+                return rs;
+            }
+        };
+        final var spliterator = Spliterators.spliteratorUnknownSize(iterator, Spliterator.ORDERED);
+        return StreamSupport.stream(spliterator, false).onClose(() -> closeResultSet(rs));
+    }
+
+    /** Safely closes the current ResultSet and starts tracking the new one. */
+    private static void closeResultSet(@Nullable final ResultSet rs) {
+        try (var oldResultSet = rs) {
+        } catch (SQLException e) {
+            throw SQLExceptionBuilder.build("Can't close a ResultSet", e);
+        }
+    }
+
+
+    @FunctionalInterface
+    public interface SqlConsumer<T> extends Consumer<T> {
+        @Override
+        default void accept(final T t) {
+            try {
+                acceptResultSet(t);
+            } catch (Exception ex) {
+                throw (ex instanceof RuntimeException re) ? re : new IllegalStateException(ex);
+            }
+        }
+        void acceptResultSet(T t) throws Exception;
+    }
+
+    /** A subclass of the undeclared exception class {@link IllegalStateException}. */
+    public static final class SqlException extends org.ujorm.tools.jdbc.SQLException {
+        private SqlException(Throwable cause, String... messages) {
+            super((messages.length > 0 || cause == null)
+                    ? String.join(" ", messages)
+                    : cause.getMessage(), cause);
+        }
+    }
+}
