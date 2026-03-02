a91606e925a948523222c74fd11d676a76e5fcbc
pavel.ponec <pavel.ponec@gmail.com>
2026-03-02 10:46:05 +0100

    ResultSetMapper: Domain -> Ujo

---
+++ project-m2/ujo-jdbc-mapper/src/test/java/org/ujorm/mapper/core/EntityManagerSelectTest.java
@@ -0,0 +1,33 @@
+package org.ujorm.mapper.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.ujorm.mapper.demo.City;
+import org.ujorm.mapper.demo.Employee;
+
+import java.time.LocalDate;
+
+/** Tests for partial updates using SnapshotProvider */
+class EntityManagerSelectTest extends AbstractDaoTest {
+
+    @Test
+    void selectTest() {
+        var cityDao = EntityManager.of(City.class, dbConnection, Long.class);
+        var city = cityDao.insert(new City(null, "California", "US", 36.7783, -119.4179));
+        var emplDao = EntityManager.of(Employee.class, dbConnection, Long.class);
+        var employee1 = emplDao.insert(createEmployee("EmplA", city));
+
+        var emplReloaded = emplDao.readNullable(employee1.getId());
+        Assertions.assertNotNull(emplReloaded);
+    }
+
+    /** Create a new Employee without ID */
+    public Employee createEmployee(String name, City city) {
+        return createEmployee(null, name, city);
+    }
+
+    /** Create a new Employee with ID */
+    public Employee createEmployee(Long id, String name, City city) {
+        return Employee.of(id, name, null, city, LocalDate.of(2020, 1, 1), true);
+    }
+}
