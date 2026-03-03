6bd93f57974e1aea0ed5bd185bcdd755fdb24d9d
pavel.ponec <pavel.ponec@gmail.com>
2026-03-03 08:42:59 +0100

    EntityManager code cleaning

--- project-m2/ujo-jdbc-mapper/src/main/java/org/ujorm/mapper/impl/Config.java
+++ project-m2/ujo-jdbc-mapper/src/main/java/org/ujorm/mapper/impl/Config.java
@@ -20,12 +20,9 @@ import java.util.function.Function;
 @Setter @Getter @ToString
 public class Config implements MapperContext {
 
-    public static final Map<Class<?>, Function<String,?>> DEFAULT_CONVERTER_MAP = initConverterMap();
-
     /** The first key in the sequence represents the primary key. */
     private boolean firstPropertyIsIdentifier = true;
 
-    private Map<Class<?>, Function<String,?>> converterMap = DEFAULT_CONVERTER_MAP;
 
     /** Maximum size of the cache in the ResultSet Mapper */
     private int maxCacheSize = 512;
@@ -41,24 +38,6 @@ public class Config implements MapperContext {
 
     /** Write a warning if the column is not a relation and has no JDBC mapping. */
     boolean columnMappingWarning = true;
-
-    @NotNull
-    static final Map<Class<?>, Function<String,?>> initConverterMap() {
-        var result = new MapBuilder();
-        {
-            result.putAllPrimitiveTypes();
-            result.put(UUID.class, UUID::fromString);
-            result.put(LocalTime.class, LocalTime::parse);
-            result.put(LocalDate.class, LocalDate::parse);
-            result.put(LocalDateTime.class, LocalDateTime::parse);
-            result.put(BigInteger.class, BigInteger::new);
-            result.put(BigDecimal.class, BigDecimal::new);
-            result.put(Currency.class, Currency::getInstance);
-            result.put(URI.class, URI::create);
-            result.put(String.class, Function.identity());
-        }
-        return result.build();
-    }
 
     /** Temporary class for building an immutable map */
     private static final class MapBuilder {
