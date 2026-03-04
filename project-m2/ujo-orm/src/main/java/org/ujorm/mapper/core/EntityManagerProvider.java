13c706b92d6110f5219b5c51c44d75fc231aa0fb
pavel.ponec <pavel.ponec@gmail.com>
2026-03-04 21:08:46 +0100

    EntityManagerProvider

---
+++ project-m2/ujo-orm/src/main/java/org/ujorm/mapper/core/EntityManagerProvider.java
@@ -0,0 +1,39 @@
+package org.ujorm.mapper.core;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Singleton to provide EntityManagers.
+ * This implementation is thread-safe using the Initialization-on-demand holder idiom.
+ */
+public final class EntityManagerProvider {
+
+    /** Private constructor to prevent instantiation. */
+    private EntityManagerProvider() {
+    }
+
+    /**
+     * Holder class for lazy-loading the singleton instance.
+     */
+    private static final class Holder {
+        private static final EntityManagerService INSTANCE = new EntityManagerService();
+    }
+
+    /**
+     * Provides the EntityService instance.
+     * @return EntityService
+     */
+    public static EntityManagerService provider() {
+        return Holder.INSTANCE;
+    }
+
+    /**
+     * Get the Entity Manager.
+     * @param domainClass An original domain class
+     * @return Entity
+     */
+    public static <D,V> EntityManager<D,V> getService(@NotNull Class<D> domainClass, @Nullable Class<V> value) {
+        return (EntityManager<D,V>) provider().entityManager(domainClass);
+    }
+}
