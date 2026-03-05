17d9077bf8704eea2431d1ea47c4fa6643794e57
pavel.ponec <pavel.ponec@gmail.com>
2026-03-05 19:23:06 +0100

    Remove obsolete libraries

--- samples/generated/src/main/java/org/version1/bo/Item.java
+++
@@ -1,8 +0,0 @@
-/** Editing of the class is allowed. */
-
-package org.version1.bo;
-import org.version1.bo.gen._Item;
-
-final public class Item extends _Item {
-
-}
--- samples/generated/src/main/java/org/version1/bo/MyProcedure.java
+++
@@ -1,8 +0,0 @@
-/** Editing of the class is allowed. */
-
-package org.version1.bo;
-import org.version1.bo.gen._MyProcedure;
-
-final public class MyProcedure extends _MyProcedure {
-
-}
