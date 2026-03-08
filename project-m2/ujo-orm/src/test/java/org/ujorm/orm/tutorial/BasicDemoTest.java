3524e33951dfc013677a707eaa161bddb63f3332
pavel.ponec <pavel.ponec@gmail.com>
2026-03-08 13:31:27 +0100

    SELECT COUNT(*)

--- project-m2/ujo-orm/src/test/java/org/ujorm/orm/tutorial/BasicDemoTest.java
+++ project-m2/ujo-orm/src/test/java/org/ujorm/orm/tutorial/BasicDemoTest.java
@@ -38,10 +38,10 @@ public class BasicDemoTest extends AbstractDemo {
         employeeCrud.insertBatch(emplDave, emplCarol);
     }
 
+    /** Safe aliasing using generated Meta classes prevents SQL typos */
     @Test
     @Order(200)
     void selectWithTypeSafeLabels() {
-        // Safe aliasing using generated Meta classes prevents SQL typos
         var sql = """
                  SELECT e.id      AS ${e.id}
                  , e.name         AS ${e.name}
@@ -95,16 +95,21 @@ public class BasicDemoTest extends AbstractDemo {
         var employeeCrud = EMPLOYEE_EM.crud(connection());
 
         var allEmployees = employeeCrud
-                .selectWhere("id > :id", builder -> builder
-                        .bind("id", 0L)
+                .selectWhere("id > :employeeId", builder -> builder
+                        .bind("employeeId", 0L)
                         .streamMap(EMPLOYEE_EM::map)
                         .sorted(Comparator.comparing(e -> e.getBoss() == null))
                         .toList());
 
         employeeCrud.delete(allEmployees.stream());
 
-        var count = employeeCrud.selectWhere("1=1", b -> b.streamMap(EMPLOYEE_EM::map).count());
-        assertEquals(0L, count);
+        var count = SqlParamBuilder.run(connection(), build -> build
+                .sql("SELECT COUNT(*) FROM employee WHERE id >= :employeeId")
+                .bind("employeeId", 0L)
+                .streamMap(rs -> rs.getInt(1))
+                .findFirst()
+                .orElseThrow());
+        assertEquals(0, count);
     }
 
     /** Create all database tables first */
